acc49a31ffb04e4bd2909ac321b7c3b206c5320a
Gustav Schauwecker <gschauwecker@simpago.com>
2021-11-02 16:56:48 +0100

    Don't run QT tests in Github Actions

--- ci/test.sh
+++ ci/test.sh
@@ -22,21 +22,6 @@ fi
 set -o nounset
 set -o xtrace
 
-# Alpine doesn't offer an xvfb
-xvfb_run_()
-{
-    INIT_DELAY_SEC=3
-
-    Xvfb :2 -screen 0 1024x768x24 &
-    xvfb_pid=$!
-    sleep ${INIT_DELAY_SEC}
-    DISPLAY=:2 ${TIMEOUT_CMD} ${TIMEOUT_TIME_ARG} ${TIMEOUT_DEFAULT} $@
-    res=${?}
-    kill ${xvfb_pid}
-
-    return ${res}
-}
-
 run_tests()
 {
     # when busybox pretends to be bash it needs different args for the timeout builtin
@@ -53,18 +38,14 @@ run_tests()
     ${TIMEOUT_CMD} ${TIMEOUT_TIME_ARG} ${TIMEOUT_DEFAULT} ./rpc_test
     rpc_test_res=${?}
 
-    xvfb_run_ ./qt_test
-    qt_test_res=${?}
-
     (cd ../systest && export NANO_NODE_EXE=../build/nano_node && ${TIMEOUT_CMD} ${TIMEOUT_TIME_ARG} 300 ./RUNALL)
     sys_test_res=${?}
 
     echo "Core Test return code: ${core_test_res}"
     echo "RPC  Test return code: ${rpc_test_res}"
-    echo "QT Test return code: ${qt_test_res}"
     echo "Sys Test return code: ${sys_test_res}"
 
-    if [[ ${core_test_res} != 0 || ${rpc_test_res} != 0 || ${qt_test_res} != 0 || ${sys_test_res} != 0 ]]; then
+    if [[ ${core_test_res} != 0 || ${rpc_test_res} != 0 || ${sys_test_res} != 0 ]]; then
         return 1
     else
         return 0

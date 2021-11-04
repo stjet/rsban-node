b9611d2e9701b1d1cbb26658e915e0cba365f624
Gustav Schauwecker <gschauwecker@simpago.com>
2021-11-04 14:51:25 +0100

    Move Account code into own file

---
+++ rust/rsnano/src/numbers/account.rs
@@ -0,0 +1,303 @@
+use super::PublicKey;
+use crate::utils::{Blake2b, RustBlake2b, Stream};
+use anyhow::Result;
+use primitive_types::U512;
+
+#[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
+pub struct Account {
+    public_key: PublicKey,
+}
+
+impl Account {
+    pub fn new() -> Self {
+        Self {
+            public_key: PublicKey::new(),
+        }
+    }
+
+    pub fn from_public_key(public_key: PublicKey) -> Self {
+        Self { public_key }
+    }
+
+    pub fn from_bytes(bytes: [u8; 32]) -> Account {
+        Self {
+            public_key: PublicKey::from_be_bytes(bytes),
+        }
+    }
+
+    pub const fn serialized_size() -> usize {
+        PublicKey::serialized_size()
+    }
+
+    pub fn serialize(&self, stream: &mut impl Stream) -> Result<()> {
+        self.public_key.serialize(stream)
+    }
+
+    pub fn deserialize(&mut self, stream: &mut impl Stream) -> Result<()> {
+        self.public_key.deserialize(stream)
+    }
+
+    pub fn to_bytes(self) -> [u8; 32] {
+        self.public_key.to_be_bytes()
+    }
+
+    pub fn as_bytes(&'_ self) -> &'_ [u8; 32] {
+        self.public_key.as_bytes()
+    }
+
+    pub fn encode_account(&self) -> String {
+        let mut number = U512::from_big_endian(self.public_key.as_bytes());
+        let check = U512::from_little_endian(&self.account_checksum());
+        number <<= 40;
+        number = number | check;
+
+        let mut result = String::with_capacity(65);
+
+        for _i in 0..60 {
+            let r = number.byte(0) & 0x1f_u8;
+            number >>= 5;
+            result.push(account_encode(r));
+        }
+        result.push_str("_onan"); // nano_
+        result.chars().rev().collect()
+    }
+
+    fn account_checksum(&self) -> [u8; 5] {
+        let mut blake = RustBlake2b::new();
+        let mut check = [0u8; 5];
+        blake.init(5).unwrap();
+        blake.update(self.public_key.as_bytes()).unwrap();
+        blake.finalize(&mut check).unwrap();
+        check
+    }
+
+    pub fn decode_account(source: &str) -> Option<Account> {
+        EncodedAccountStr(source)
+            .to_u512()
+            .map(|encoded| encoded.to_account())
+            .flatten()
+    }
+
+    pub fn decode_hex(s: &str) -> Option<Self> {
+        if s.is_empty() || s.len() > 64 {
+            return None;
+        }
+
+        let mut bytes = [0u8; 32];
+        match hex::decode_to_slice(s, &mut bytes) {
+            Ok(_) => Some(Account::from_bytes(bytes)),
+            Err(_) => None,
+        }
+    }
+}
+
+struct EncodedAccountU512(U512);
+
+impl EncodedAccountU512 {
+    fn account_bytes(&self) -> [u8; 32] {
+        let mut bytes_512 = [0u8; 64];
+        (self.0 >> 40).to_big_endian(&mut bytes_512);
+        let mut bytes_256 = [0u8; 32];
+        bytes_256.copy_from_slice(&bytes_512[32..]);
+        bytes_256
+    }
+
+    fn checksum_bytes(&self) -> [u8; 5] {
+        [
+            self.0.byte(0),
+            self.0.byte(1),
+            self.0.byte(2),
+            self.0.byte(3),
+            self.0.byte(4),
+        ]
+    }
+
+    fn to_account(&self) -> Option<Account> {
+        let account = Account::from_bytes(self.account_bytes());
+        if account.account_checksum() == self.checksum_bytes() {
+            Some(account)
+        } else {
+            None
+        }
+    }
+}
+
+struct EncodedAccountStr<'a>(&'a str);
+impl<'a> EncodedAccountStr<'a> {
+    fn is_valid(&self) -> bool {
+        self.0.len() > 4
+            && self.has_valid_prefix()
+            && self.is_length_valid()
+            && self.is_first_digit_valid()
+    }
+
+    fn has_valid_prefix(&self) -> bool {
+        self.has_xrb_prefix() || self.has_nano_prefix() || self.has_node_id_prefix()
+    }
+
+    fn has_xrb_prefix(&self) -> bool {
+        self.0.starts_with("xrb_") || self.0.starts_with("xrb-")
+    }
+
+    fn has_nano_prefix(&self) -> bool {
+        self.0.starts_with("nano_") || self.0.starts_with("nano-")
+    }
+
+    fn has_node_id_prefix(&self) -> bool {
+        self.0.starts_with("node_")
+    }
+
+    fn is_length_valid(&self) -> bool {
+        if self.has_xrb_prefix() && self.0.chars().count() != 64 {
+            return false;
+        }
+        if self.has_nano_prefix() && self.0.chars().count() != 65 {
+            return false;
+        }
+        true
+    }
+
+    fn prefix_len(&self) -> usize {
+        if self.has_xrb_prefix() {
+            4
+        } else {
+            5
+        }
+    }
+
+    fn first_digit(&self) -> Option<char> {
+        self.0.chars().nth(self.prefix_len())
+    }
+
+    fn is_first_digit_valid(&self) -> bool {
+        match self.first_digit() {
+            Some('1') | Some('3') => true,
+            _ => false,
+        }
+    }
+
+    fn chars_after_prefix(&'_ self) -> impl Iterator<Item = char> + '_ {
+        self.0.chars().skip(self.prefix_len())
+    }
+
+    fn to_u512(&self) -> Option<EncodedAccountU512> {
+        if !self.is_valid() {
+            return None;
+        }
+
+        let mut number = U512::default();
+        for character in self.chars_after_prefix() {
+            match self.decode_byte(character) {
+                Some(byte) => {
+                    number <<= 5;
+                    number = number + byte;
+                }
+                None => return None,
+            }
+        }
+        Some(EncodedAccountU512(number))
+    }
+
+    fn decode_byte(&self, character: char) -> Option<u8> {
+        if character.is_ascii() {
+            let character = character as u8;
+            if (0x30..0x80).contains(&character) {
+                let byte: u8 = account_decode(character);
+                if byte != b'~' {
+                    return Some(byte);
+                }
+            }
+        }
+
+        None
+    }
+}
+
+const ACCOUNT_LOOKUP: &[char] = &[
+    '1', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z',
+];
+
+const ACCOUNT_REVERSE: &[char] = &[
+    '~', '0', '~', '1', '2', '3', '4', '5', '6', '7', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+    '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+    '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '8', '9', ':', ';', '<', '=', '>', '?',
+    '@', 'A', 'B', '~', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', '~', 'L', 'M', 'N', 'O', '~',
+    '~', '~', '~', '~',
+];
+
+fn account_encode(value: u8) -> char {
+    ACCOUNT_LOOKUP[value as usize]
+}
+
+fn account_decode(value: u8) -> u8 {
+    let mut result = ACCOUNT_REVERSE[(value - 0x30) as usize] as u8;
+    if result != b'~' {
+        result -= 0x30;
+    }
+    result
+}
+
+impl From<u64> for Account {
+    fn from(value: u64) -> Self {
+        let mut key = PublicKey::new();
+        key.value[24..].copy_from_slice(&value.to_be_bytes());
+        Account::from_public_key(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // original test: account.encode_zero
+    #[test]
+    fn encode_zero() {
+        let account = Account::new();
+        let encoded = account.encode_account();
+        assert_eq!(
+            encoded,
+            "nano_1111111111111111111111111111111111111111111111111111hifc8npp"
+        );
+        let copy = Account::decode_account(&encoded).expect("decode failed");
+        assert_eq!(account, copy);
+    }
+
+    // original test: account.encode_all
+    #[test]
+    fn encode_all() {
+        let account = Account::from_bytes([0xFF; 32]);
+        let encoded = account.encode_account();
+        assert_eq!(
+            encoded,
+            "nano_3zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzc3yoon41"
+        );
+        let copy = Account::decode_account(&encoded).expect("decode failed");
+        assert_eq!(account, copy);
+    }
+
+    // original test: account.encode_fail
+    #[test]
+    fn encode_fail() {
+        let account = Account::new();
+        let mut encoded = account.encode_account();
+        encoded.replace_range(16..17, "x");
+        assert!(Account::decode_account(&encoded).is_none());
+    }
+
+    #[test]
+    fn encode_real_account() {
+        let account =
+            Account::decode_hex("E7F5F39D52AC32ADF978BBCF6EA50C7A5FBBDDCADE965C542808ADAE9DEF6B20")
+                .unwrap();
+        let encoded = account.encode_account();
+        assert_eq!(
+            encoded,
+            "nano_3szoyggo7d3koqwqjgyhftkirykzqhgwoqnpdjc4i47fotgyyts1j8ab3mti"
+        );
+        assert_eq!(
+            Account::decode_account(&encoded).expect("could not decode"),
+            account
+        );
+    }
+}

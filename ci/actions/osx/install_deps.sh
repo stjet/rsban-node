dafd5816bb10200b79d0cefbea143216f19712ad
Gustav Schauwecker <service@simpago.com>
2022-05-20 11:09:17 +0200

    Enable OSX unit tests again

--- ci/actions/osx/install_deps.sh
+++ ci/actions/osx/install_deps.sh
@@ -2,6 +2,11 @@
 
 brew update
 brew install coreutils
+brew install rust
 brew cask install xquartz
 sudo util/build_prep/fetch_boost.sh
 util/build_prep/macosx/build_qt.sh
+git clone https://github.com/corrosion-rs/corrosion.git
+cmake -Scorrosion -Bbuild -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+cmake --install build --config Release

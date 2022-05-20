d0dae1e5470c11f17f78944c29fe159fd474f497
Gustav Schauwecker <service@simpago.com>
2022-05-20 09:31:31 +0200

    Fix docker image tags

--- ci/actions/linux/install_deps.sh
+++ ci/actions/linux/install_deps.sh
@@ -2,10 +2,10 @@
 
 sudo mkdir -p /etc/docker && echo '{"ipv6":true,"fixed-cidr-v6":"2001:db8:1::/64"}' | sudo tee /etc/docker/daemon.json && sudo service docker restart
 
-ci/build-docker-image.sh docker/ci/Dockerfile-base simpago/nano-env:base
+ci/build-docker-image.sh docker/ci/Dockerfile-base simpago/rsnano-env:base
 if [[ "${COMPILER:-}" != "" ]]; then
-    ci/build-docker-image.sh docker/ci/Dockerfile-gcc simpago/nano-env:${COMPILER}
+    ci/build-docker-image.sh docker/ci/Dockerfile-gcc simpago/rsnano-env:${COMPILER}
 else
-    ci/build-docker-image.sh docker/ci/Dockerfile-gcc simpago/nano-env:gcc
-    ci/build-docker-image.sh docker/ci/Dockerfile-clang-6 simpago/nano-env:clang-6
+    ci/build-docker-image.sh docker/ci/Dockerfile-gcc simpago/rsnano-env:gcc
+    ci/build-docker-image.sh docker/ci/Dockerfile-clang-6 simpago/rsnano-env:clang-6
 fi

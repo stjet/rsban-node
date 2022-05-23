c89f78e31adba3284b652b06f25131c7e5b2b191
Gustav Schauwecker <service@simpago.com>
2022-05-23 07:02:32 +0200

    Fix osx CI build

--- ci/actions/osx/install_deps.sh
+++ ci/actions/osx/install_deps.sh
@@ -7,6 +7,6 @@ brew cask install xquartz
 sudo util/build_prep/fetch_boost.sh
 util/build_prep/macosx/build_qt.sh
 git clone https://github.com/corrosion-rs/corrosion.git
-cmake -Scorrosion -Bbuild -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config Release
-cmake --install build --config Release
+cmake -Scorrosion -Bcorrosion_build -DCMAKE_BUILD_TYPE=Release
+cmake --build corrosion_build --config Release
+sudo cmake --install corrosion_build --config Release

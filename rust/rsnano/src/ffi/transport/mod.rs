85dd39dccfc93536bff4fd3dab43893f03a652a5
Gustav Schauwecker <service@simpago.com>
2022-07-18 10:50:03 +0200

    Refactor: Move channel_tcp_observer FFI to own file

--- rust/rsnano/src/ffi/transport/mod.rs
+++ rust/rsnano/src/ffi/transport/mod.rs
@@ -1,7 +1,8 @@
 mod channel;
 mod channel_tcp;
+mod channel_tcp_observer;
 mod socket;
 mod tcp_channels;
 
-pub use channel_tcp::ChannelTcpObserverWeakPtr;
+pub use channel_tcp_observer::ChannelTcpObserverWeakPtr;
 pub use socket::{EndpointDto, SocketHandle};

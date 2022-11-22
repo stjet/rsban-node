6c01b2d096ab620ef902e2dbac2c8ffb19d410a8
Gustav Schauwecker <service@simpago.com>
2022-11-22 10:41:42 +0100

    Port epoch unit tests

--- rust/rsnano/src/ledger/ledger_tests/epoch_v1.rs
+++ rust/rsnano/src/ledger/ledger_tests/epoch_v1.rs
@@ -1,7 +1,7 @@
 use crate::{
     core::{
-        Account, Amount, Block, BlockBuilder, BlockDetails, Epoch, KeyPair, SignatureVerification,
-        StateBlockBuilder,
+        Account, Amount, Block, BlockBuilder, BlockDetails, Epoch, KeyPair, PendingKey,
+        SignatureVerification, StateBlockBuilder,
     },
     ledger::{ProcessResult, DEV_GENESIS_KEY},
     DEV_CONSTANTS, DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH,
@@ -277,4 +277,241 @@ fn can_add_state_receive_block_after_epoch1() {
         BlockDetails::new(Epoch::Epoch1, false, true, false)
     );
     assert_eq!(receive.sideband().unwrap().source_epoch, Epoch::Epoch1);
+}
+
+#[test]
+fn epoch_blocks_receive_upgrade() {
+    let ctx = LedgerContext::empty();
+    let mut txn = ctx.ledger.rw_txn();
+    let destination = KeyPair::new();
+    let destination_account = Account::from(destination.public_key());
+
+    let mut send1 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(*DEV_GENESIS_HASH)
+        .representative(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(50))
+        .link(destination_account)
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send1);
+
+    let mut epoch1 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(send1.hash())
+        .representative(*DEV_GENESIS_ACCOUNT)
+        .balance(send1.balance())
+        .link(ctx.ledger.epoch_link(Epoch::Epoch1).unwrap())
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut epoch1);
+
+    let mut send2 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(epoch1.hash())
+        .representative(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(100))
+        .link(destination_account)
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send2);
+
+    let mut open1 = BlockBuilder::open()
+        .source(send1.hash())
+        .representative(destination_account)
+        .account(destination_account)
+        .sign(&destination)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut open1);
+
+    let mut receive1 = BlockBuilder::receive()
+        .previous(open1.hash())
+        .source(send2.hash())
+        .sign(&destination)
+        .build()
+        .unwrap();
+    let result = ctx
+        .ledger
+        .process(txn.as_mut(), &mut receive1, SignatureVerification::Unknown);
+
+    assert_eq!(result.code, ProcessResult::Unreceivable);
+
+    let mut receive2 = BlockBuilder::state()
+        .account(destination_account)
+        .previous(open1.hash())
+        .representative(destination_account)
+        .balance(Amount::new(100))
+        .link(send2.hash())
+        .sign(&destination)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut receive2);
+
+    assert_eq!(receive2.sideband().unwrap().details.epoch, Epoch::Epoch1);
+    assert_eq!(receive2.sideband().unwrap().source_epoch, Epoch::Epoch1);
+
+    let destination_info = ctx
+        .ledger
+        .store
+        .account()
+        .get(txn.txn(), &destination_account)
+        .unwrap();
+    assert_eq!(destination_info.epoch, Epoch::Epoch1);
+
+    ctx.ledger
+        .rollback(txn.as_mut(), &receive2.hash(), &mut Vec::new())
+        .unwrap();
+
+    let destination_info = ctx
+        .ledger
+        .store
+        .account()
+        .get(txn.txn(), &destination_account)
+        .unwrap();
+    assert_eq!(destination_info.epoch, Epoch::Epoch0);
+
+    let pending_send2 = ctx
+        .ledger
+        .store
+        .pending()
+        .get(
+            txn.txn(),
+            &PendingKey::new(destination_account, send2.hash()),
+        )
+        .unwrap();
+    assert_eq!(pending_send2.epoch, Epoch::Epoch1);
+
+    ctx.process(txn.as_mut(), &mut receive2);
+
+    let destination2 = KeyPair::new();
+    let destination2_account = Account::from(destination2.public_key());
+    let mut send3 = BlockBuilder::state()
+        .account(destination_account)
+        .previous(receive2.hash())
+        .representative(destination_account)
+        .balance(Amount::new(50))
+        .link(destination2_account)
+        .sign(&destination)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send3);
+
+    let mut open2 = BlockBuilder::open()
+        .source(send3.hash())
+        .representative(destination2_account)
+        .account(destination2_account)
+        .sign(&destination2)
+        .build()
+        .unwrap();
+    let result = ctx
+        .ledger
+        .process(txn.as_mut(), &mut open2, SignatureVerification::Unknown);
+    assert_eq!(result.code, ProcessResult::Unreceivable);
+
+    // Upgrade to epoch 2 and send to destination. Try to create an open block from an epoch 2 source block.
+    let destination3 = KeyPair::new();
+    let destination3_account = Account::from(destination3.public_key());
+    let mut epoch2 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(send2.hash())
+        .representative(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(100))
+        .link(ctx.ledger.epoch_link(Epoch::Epoch2).unwrap())
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut epoch2);
+    let mut send4 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(epoch2.hash())
+        .account(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(150))
+        .link(destination3_account)
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send4);
+
+    let mut open3 = BlockBuilder::open()
+        .source(send4.hash())
+        .representative(destination3_account)
+        .account(destination3_account)
+        .sign(&destination3)
+        .build()
+        .unwrap();
+    let result = ctx
+        .ledger
+        .process(txn.as_mut(), &mut open3, SignatureVerification::Unknown);
+    assert_eq!(result.code, ProcessResult::Unreceivable);
+
+    // Send it to an epoch 1 account
+    let mut send5 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(send4.hash())
+        .account(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(200))
+        .link(destination_account)
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send5);
+
+    let destination_info = ctx
+        .ledger
+        .store
+        .account()
+        .get(txn.txn(), &destination_account)
+        .unwrap();
+    assert_eq!(destination_info.epoch, Epoch::Epoch1);
+
+    let mut receive3 = BlockBuilder::state()
+        .account(destination_account)
+        .previous(send3.hash())
+        .representative(destination_account)
+        .balance(100)
+        .link(send5.hash())
+        .sign(&destination)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut receive3);
+    assert_eq!(receive3.sideband().unwrap().details.epoch, Epoch::Epoch2);
+    assert_eq!(receive3.sideband().unwrap().source_epoch, Epoch::Epoch2);
+    let destination_info = ctx
+        .ledger
+        .store
+        .account()
+        .get(txn.txn(), &destination_account)
+        .unwrap();
+    assert_eq!(destination_info.epoch, Epoch::Epoch2);
+
+    // Upgrade an unopened account straight to epoch 2
+    let destination4 = KeyPair::new();
+    let destination4_account = Account::from(destination4.public_key());
+    let mut send6 = BlockBuilder::state()
+        .account(*DEV_GENESIS_ACCOUNT)
+        .previous(send5.hash())
+        .representative(*DEV_GENESIS_ACCOUNT)
+        .balance(DEV_CONSTANTS.genesis_amount - Amount::new(250))
+        .link(destination4_account)
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut send6);
+
+    let mut epoch4 = BlockBuilder::state()
+        .account(destination4_account)
+        .previous(0)
+        .representative(0)
+        .balance(0)
+        .link(ctx.ledger.epoch_link(Epoch::Epoch2).unwrap())
+        .sign(&DEV_GENESIS_KEY)
+        .build()
+        .unwrap();
+    ctx.process(txn.as_mut(), &mut epoch4);
+    assert_eq!(epoch4.sideband().unwrap().details.epoch, Epoch::Epoch2);
+    assert_eq!(epoch4.sideband().unwrap().source_epoch, Epoch::Epoch0); // Not used for epoch blocks
 }

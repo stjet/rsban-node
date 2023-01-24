6ef68e3f6a205e271dcbfc9946dce1fa1ff1bfa4
Gustav Schauwecker <service@simpago.com>
2023-01-24 08:56:03 +0100

    Refactor confirmation_height_unbounded

--- rust/node/src/confirmation_height/confirmation_height_unbounded.rs
+++ rust/node/src/confirmation_height/confirmation_height_unbounded.rs
@@ -335,6 +335,104 @@ impl ConfirmationHeightUnbounded {
         self.cement_queue.len() >= UNBOUNDED_CUTOFF
     }
 
+    /// Walks backwards through the accounts blocks (starting at current_block)
+    /// and finds all unconfirmed receives and adds them to receive_source_pairs
+    pub fn collect_unconfirmed_receive_and_sources_for_account(
+        &mut self,
+        txn: &dyn Transaction,
+        current_block: &Arc<BlockEnum>,
+        confirmation_height: u64,
+        receive_source_pairs: &mut Vec<Arc<ReceiveSourcePair>>,
+        block_callback_data: &mut Vec<BlockHash>,
+        orig_block_callback_data: &mut Vec<BlockHash>,
+        original_block: &BlockEnum,
+    ) {
+        let account = current_block.account_calculated();
+        let mut block_hash = current_block.hash();
+        let mut num_to_confirm = current_block.sideband().unwrap().height - confirmation_height;
+
+        // Handle any sends above a receive
+        let mut is_original_block = block_hash == original_block.hash();
+        let mut hit_receive = false;
+        let mut first_iter = true;
+        while (num_to_confirm > 0) && !block_hash.is_zero() && !self.stopped.load(Ordering::SeqCst)
+        {
+            let current_block = if first_iter {
+                self.cache_block(Arc::clone(&current_block));
+                Some(Arc::clone(current_block))
+            } else {
+                self.get_block_and_sideband(&block_hash, txn)
+            };
+
+            if let Some(current_block) = &current_block {
+                if self.is_receive_block(txn, current_block) {
+                    if !hit_receive && !block_callback_data.is_empty() {
+                        // Add the callbacks to the associated receive to retrieve later
+                        let last_pair = receive_source_pairs.last().unwrap();
+                        last_pair
+                            .receive_details
+                            .lock()
+                            .unwrap()
+                            .source_block_callback_data = block_callback_data.clone();
+                        block_callback_data.clear();
+                    }
+
+                    is_original_block = false;
+                    hit_receive = true;
+
+                    let details = ConfHeightDetails {
+                        account,
+                        latest_confirmed_block: block_hash,
+                        new_height: confirmation_height + num_to_confirm,
+                        num_blocks_confirmed: 1,
+                        block_callback_data: vec![block_hash],
+                        source_block_callback_data: Vec::new(),
+                    };
+                    receive_source_pairs.push(Arc::new(ReceiveSourcePair {
+                        receive_details: Arc::new(Mutex::new(details)),
+                        source_hash: current_block.source_or_link(),
+                    }));
+                } else if is_original_block {
+                    orig_block_callback_data.push(block_hash);
+                } else {
+                    if !hit_receive {
+                        // This block is cemented via a receive, as opposed to below a receive being cemented
+                        block_callback_data.push(block_hash);
+                    } else {
+                        // We have hit a receive before, add the block to it
+                        let last_pair = receive_source_pairs.last().unwrap();
+                        let last_receive_details = &last_pair.receive_details;
+                        let mut last_receive_details_lock = last_receive_details.lock().unwrap();
+                        last_receive_details_lock.num_blocks_confirmed += 1;
+                        last_receive_details_lock
+                            .block_callback_data
+                            .push(block_hash);
+                        drop(last_receive_details_lock);
+
+                        self.implicit_receive_cemented_mapping
+                            .add(block_hash, last_receive_details);
+                    }
+                }
+
+                block_hash = current_block.previous();
+            }
+
+            num_to_confirm -= 1;
+            first_iter = false;
+        }
+    }
+
+    fn is_receive_block(&self, txn: &dyn Transaction, block: &Arc<BlockEnum>) -> bool {
+        let source = block.source_or_link();
+
+        // a receive block must have a source
+        !source.is_zero()
+            && !self.ledger.is_epoch_link(&source.into())
+            // if source does not point to an existing block then it
+            // must be a send block
+            && self.ledger.store.block().exists(txn, &source)
+    }
+
     pub fn prepare_iterated_blocks_for_cementing(
         &mut self,
         preparation_data_a: &mut PreparationData,
@@ -464,96 +562,6 @@ impl ConfirmationHeightUnbounded {
             self.cement_queue.push(receive_details_lock.clone())
         }
     }
-
-    pub fn collect_unconfirmed_receive_and_sources_for_account(
-        &mut self,
-        txn: &dyn Transaction,
-        block_a: &Arc<BlockEnum>,
-        confirmation_height: u64,
-        receive_source_pairs: &mut Vec<Arc<ReceiveSourcePair>>,
-        block_callback_data: &mut Vec<BlockHash>,
-        orig_block_callback_data: &mut Vec<BlockHash>,
-        original_block: &BlockEnum,
-    ) {
-        let mut block_hash = block_a.hash();
-        let mut num_to_confirm = block_a.sideband().unwrap().height - confirmation_height;
-
-        // Handle any sends above a receive
-        let mut is_original_block = block_hash == original_block.hash();
-        let mut hit_receive = false;
-        let mut first_iter = true;
-        while (num_to_confirm > 0) && !block_hash.is_zero() && !self.stopped.load(Ordering::SeqCst)
-        {
-            let block = if first_iter {
-                self.cache_block(Arc::clone(&block_a));
-                Some(Arc::clone(block_a))
-            } else {
-                self.get_block_and_sideband(&block_hash, txn)
-            };
-
-            if let Some(block) = &block {
-                let source = block.source_or_link();
-
-                if !source.is_zero()
-                    && !self.ledger.is_epoch_link(&source.into())
-                    && self.ledger.store.block().exists(txn, &source)
-                {
-                    if !hit_receive && !block_callback_data.is_empty() {
-                        // Add the callbacks to the associated receive to retrieve later
-                        let last_pair = receive_source_pairs.last().unwrap();
-                        last_pair
-                            .receive_details
-                            .lock()
-                            .unwrap()
-                            .source_block_callback_data = block_callback_data.clone();
-                        block_callback_data.clear();
-                    }
-
-                    is_original_block = false;
-                    hit_receive = true;
-
-                    let details = ConfHeightDetails {
-                        account: block_a.account_calculated(),
-                        latest_confirmed_block: block_hash,
-                        new_height: confirmation_height + num_to_confirm,
-                        num_blocks_confirmed: 1,
-                        block_callback_data: vec![block_hash],
-                        source_block_callback_data: Vec::new(),
-                    };
-                    receive_source_pairs.push(Arc::new(ReceiveSourcePair {
-                        receive_details: Arc::new(Mutex::new(details)),
-                        source_hash: source,
-                    }));
-                } else if is_original_block {
-                    orig_block_callback_data.push(block_hash);
-                } else {
-                    if !hit_receive {
-                        // This block is cemented via a recieve, as opposed to below a receive being cemented
-                        block_callback_data.push(block_hash);
-                    } else {
-                        // We have hit a receive before, add the block to it
-                        let last_pair = receive_source_pairs.last().unwrap();
-                        let last_receive_details = &last_pair.receive_details;
-                        let mut last_receive_details_lock = last_receive_details.lock().unwrap();
-                        last_receive_details_lock.num_blocks_confirmed += 1;
-                        last_receive_details_lock
-                            .block_callback_data
-                            .push(block_hash);
-                        drop(last_receive_details_lock);
-
-                        self.implicit_receive_cemented_mapping
-                            .add(block_hash, last_receive_details);
-                    }
-                }
-
-                block_hash = block.previous();
-            }
-
-            num_to_confirm -= 1;
-            first_iter = false;
-        }
-    }
-
     pub fn cement_pending_blocks(&mut self) {
         self.cementor.cement_blocks(
             &mut self.cement_queue,

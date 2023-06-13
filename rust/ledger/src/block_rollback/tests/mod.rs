875b5e4530b25a329967cbb7418a8f63bb820848
Gustav Schauwecker <service@simpago.com>
2023-06-13 16:57:45 +0200

    Refactor rollback unit tests

--- rust/ledger/src/block_rollback/tests/mod.rs
+++ rust/ledger/src/block_rollback/tests/mod.rs
@@ -1,1 +1,51 @@
+use rsnano_core::{Account, BlockHash, ConfirmationHeightInfo, TestAccountChain};
+
+use crate::ledger_constants::LEDGER_CONSTANTS_STUB;
+
+use super::rollback_planner::{RollbackInstructions, RollbackPlanner, RollbackStep};
+
 mod rollback_epoch;
+
+pub(crate) struct RollbackTest<'a> {
+    planner: RollbackPlanner<'a>,
+}
+
+impl<'a> RollbackTest<'a> {
+    pub fn for_chain(chain: &'a TestAccountChain) -> Self {
+        Self {
+            planner: create_test_rollback_planner(chain),
+        }
+    }
+
+    pub fn with_linked_account(mut self, account: impl Into<Account>) -> Self {
+        self.planner.linked_account = account.into();
+        self
+    }
+
+    pub fn assert_rollback_succeeds(self) -> RollbackInstructions {
+        let result = self
+            .planner
+            .roll_back_head_block()
+            .expect("rollback should succeed");
+        let RollbackStep::RollBackBlock(instructions) = result else { panic!("expected RollBackBlock") };
+        instructions
+    }
+}
+
+fn create_test_rollback_planner<'a>(chain: &'a TestAccountChain) -> RollbackPlanner<'a> {
+    RollbackPlanner {
+        epochs: &LEDGER_CONSTANTS_STUB.epochs,
+        head_block: chain.latest_block(),
+        account: chain.account(),
+        current_account_info: chain.account_info(),
+        previous_representative: None,
+        previous: Some(chain.block(chain.height() - 1).clone()),
+        linked_account: Account::zero(),
+        pending_receive: None,
+        latest_block_for_destination: None,
+        confirmation_height: ConfirmationHeightInfo {
+            height: 0,
+            frontier: BlockHash::zero(),
+        },
+    }
+}
--- rust/ledger/src/block_rollback/tests/rollback_epoch.rs
+++ rust/ledger/src/block_rollback/tests/rollback_epoch.rs
@@ -1,38 +1,13 @@
-use rsnano_core::{
-    Account, Amount, BlockHash, ConfirmationHeightInfo, Epoch, PendingInfo, PendingKey,
-    TestAccountChain,
-};
-
-use crate::{
-    block_rollback::rollback_planner::{RollbackPlanner, RollbackStep},
-    ledger_constants::LEDGER_CONSTANTS_STUB,
-};
+use crate::block_rollback::tests::RollbackTest;
+use rsnano_core::{Account, Amount, Epoch, PendingInfo, PendingKey, TestAccountChain};
 
 #[test]
 fn rollback_epoch1() {
     let mut chain = TestAccountChain::new_opened_chain();
     chain.add_epoch_v1();
 
-    let planner = RollbackPlanner {
-        epochs: &LEDGER_CONSTANTS_STUB.epochs,
-        head_block: chain.latest_block(),
-        account: chain.account(),
-        current_account_info: chain.account_info(),
-        previous_representative: None,
-        previous: Some(chain.block(chain.height() - 1).clone()),
-        linked_account: Account::zero(),
-        pending_receive: None,
-        latest_block_for_destination: None,
-        confirmation_height: ConfirmationHeightInfo {
-            height: 0,
-            frontier: BlockHash::zero(),
-        },
-    };
+    let instructions = RollbackTest::for_chain(&chain).assert_rollback_succeeds();
 
-    let result = planner
-        .roll_back_head_block()
-        .expect("rollback should succeed");
-    let RollbackStep::RollBackBlock(instructions) = result else { panic!("expected RollBackBlock") };
     assert_eq!(instructions.set_account_info.epoch, Epoch::Epoch0);
 }
 
@@ -41,28 +16,10 @@ fn rollback_receive_block_which_performed_epoch1_upgrade_undoes_epoch_upgrade() 
     let mut chain = TestAccountChain::new_opened_chain();
     let receive_block = chain.new_receive_block().amount_received(1).build();
     let receive_block = chain.add_block(receive_block, Epoch::Epoch1).clone();
-    let previous_block = chain.block(chain.height() - 1).clone();
 
-    let planner = RollbackPlanner {
-        epochs: &LEDGER_CONSTANTS_STUB.epochs,
-        head_block: &receive_block,
-        account: receive_block.account(),
-        current_account_info: chain.account_info(),
-        previous_representative: Some(previous_block.representative().unwrap()),
-        previous: Some(previous_block.clone()),
-        linked_account: Account::from(456),
-        pending_receive: None,
-        latest_block_for_destination: None,
-        confirmation_height: ConfirmationHeightInfo {
-            height: 0,
-            frontier: BlockHash::from(0),
-        },
-    };
-
-    let result = planner
-        .roll_back_head_block()
-        .expect("rollback should succeed");
-    let RollbackStep::RollBackBlock(instructions) = result else { panic!("expected RollBackBlock") };
+    let instructions = RollbackTest::for_chain(&chain)
+        .with_linked_account(456)
+        .assert_rollback_succeeds();
 
     assert_eq!(instructions.set_account_info.epoch, Epoch::Epoch0);
     assert_eq!(
@@ -70,7 +27,7 @@ fn rollback_receive_block_which_performed_epoch1_upgrade_undoes_epoch_upgrade() 
         Some((
             PendingKey::new(receive_block.account(), receive_block.link().into()),
             PendingInfo {
-                source: planner.linked_account,
+                source: Account::from(456),
                 amount: Amount::raw(1),
                 epoch: Epoch::Epoch1
             }
@@ -81,28 +38,10 @@ fn rollback_receive_block_which_performed_epoch1_upgrade_undoes_epoch_upgrade() 
 #[test]
 fn rollback_epoch_v2() {
     let mut chain = TestAccountChain::new_opened_chain();
-    let previous_block = chain.add_epoch_v1().clone();
-    let epoch2_block = chain.add_epoch_v2().clone();
+    chain.add_epoch_v1();
+    chain.add_epoch_v2();
 
-    let planner = RollbackPlanner {
-        epochs: &LEDGER_CONSTANTS_STUB.epochs,
-        head_block: &epoch2_block,
-        account: epoch2_block.account_calculated(),
-        current_account_info: chain.account_info(),
-        previous_representative: None,
-        previous: Some(previous_block),
-        linked_account: Account::zero(),
-        pending_receive: None,
-        latest_block_for_destination: None,
-        confirmation_height: ConfirmationHeightInfo {
-            height: 2,
-            frontier: BlockHash::from(1),
-        },
-    };
+    let instructions = RollbackTest::for_chain(&chain).assert_rollback_succeeds();
 
-    let result = planner
-        .roll_back_head_block()
-        .expect("rollback should succeed");
-    let RollbackStep::RollBackBlock(instructions) = result else { panic!("expected RollBackBlock") };
     assert_eq!(instructions.set_account_info.epoch, Epoch::Epoch1);
 }

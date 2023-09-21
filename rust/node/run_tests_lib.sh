8eb9f3e61854c27d96d338976fb92654d4d396e8
Gustav Schauwecker <service@simpago.com>
2023-09-21 09:19:03 +0200

    Run test sounds in background

--- rust/node/run_tests_lib.sh
+++ rust/node/run_tests_lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if cargo test --lib -q "$@"
 then
-	play -q ../../sounds/success.ogg
+	play -q ../../sounds/success.ogg&
 else 
-	play -q ../../sounds/failed.ogg
+	play -q ../../sounds/failed.ogg&
 fi

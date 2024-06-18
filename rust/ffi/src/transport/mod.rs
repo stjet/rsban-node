ceec20945ae47dd338a9318f23d3a72ce2a719f9
Gustav Schauwecker <service@simpago.com>
2024-06-18 11:27:35 +0200

    Rename files

--- rust/ffi/src/transport/mod.rs
+++ rust/ffi/src/transport/mod.rs
@@ -2,12 +2,12 @@ mod bandwidth_limiter;
 mod channel;
 mod channel_tcp;
 mod live_message_processor;
+mod message_processor;
 mod network_filter;
 mod network_threads;
 mod socket;
 mod syn_cookies;
 mod tcp_channels;
-mod tcp_message_manager;
 
 pub use bandwidth_limiter::OutboundBandwidthLimiterHandle;
 pub use channel::{ChannelHandle, FfiInboundCallback};
@@ -15,12 +15,12 @@ pub use channel_tcp::{
     ChannelTcpSendBufferCallback, ChannelTcpSendCallback, ChannelTcpSendCallbackWrapper,
     SendBufferCallbackWrapper,
 };
+pub use message_processor::TcpMessageManagerHandle;
 pub use network_filter::NetworkFilterHandle;
 pub use socket::{
     EndpointDto, ReadCallbackWrapper, SocketDestroyContext, SocketHandle, SocketReadCallback,
 };
 pub use syn_cookies::SynCookiesHandle;
-pub use tcp_message_manager::TcpMessageManagerHandle;
 
 pub use live_message_processor::LiveMessageProcessorHandle;
 pub use network_threads::NetworkThreadsHandle;

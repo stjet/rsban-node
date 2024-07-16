b5c1168a21c72d2018ed2194ae9773faf81f4077
Fiono11 <ruipedromorais11@gmail.com>
2024-07-16 13:27:29 +0100

    Improve generation_config command

--- rust/main/src/cli/commands/node/generate_config.rs
+++ rust/main/src/cli/commands/node/generate_config.rs
@@ -1,52 +1,49 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 use rsnano_core::work::WorkThresholds;
 use rsnano_node::{
     config::{DaemonConfig, NetworkConstants, RpcConfig},
     utils::TomlConfig,
     NetworkParams,
 };
-use std::path::PathBuf;
 
 #[derive(Parser)]
+#[command(group = ArgGroup::new("input")
+    .args(&["node", "rpc"]))]
 pub(crate) struct GenerateConfigOptions {
-    #[arg(short, long)]
-    config_type: String,
-    #[arg(short, long)]
+    #[arg(long, group = "input")]
+    node: bool,
+    #[arg(long, group = "input")]
+    rpc: bool,
+    #[arg(long)]
     use_defaults: bool,
-    #[arg(short, long)]
-    data_path: PathBuf,
 }
 
 impl GenerateConfigOptions {
     pub(crate) fn run(&self) -> anyhow::Result<()> {
-        let mut valid_type = false;
         let mut toml = TomlConfig::new();
         let network = NetworkConstants::active_network();
+        let mut config_type = "node";
 
-        if self.config_type == "node" {
-            valid_type = true;
+        if self.node {
             let network_params = NetworkParams::new(network);
             let mut config = DaemonConfig::new(&network_params, 0)?;
             config.node.peering_port = Some(network_params.network.default_node_port);
             config.serialize_toml(&mut toml)?
-        } else if self.config_type == "rpc" {
+        } else if self.rpc {
+            config_type = "rpc";
             let network_constants = NetworkConstants::new(WorkThresholds::new(0, 0, 0), network);
-            valid_type = true;
             let config = RpcConfig::new(&network_constants, 0);
             config.serialize_toml(&mut toml)?
         } else {
-            eprintln!(
-                "Invalid configuration type {}. Must be node or rpc.",
-                self.config_type
-            );
+            println!("Configuration type must be either node or rpc");
         }
 
-        if valid_type {
+        if !self.use_defaults {
             println!("# This is an example configuration file for Nano. Visit https://docs.nano.org/running-a-node/configuration/ for more information.\n#");
             println!(
                 "# Fields may need to be defined in the context of a [category] above them.\n"
             );
-            println!("# The desired configuration changes should be placed in config-{}.toml in the node data path.\n", self.config_type);
+            println!("# The desired configuration changes should be placed in config-{}.toml in the node data path.\n", config_type);
             println!("# To change a value from its default, uncomment (erasing #) the corresponding field.\n");
             println!("# It is not recommended to uncomment every field, as the default value for important fields may change in the future. Only change what you need.\n");
             println!("# Additional information for notable configuration options is available in https://docs.nano.org/running-a-node/configuration/#notable-configuration-options\n");

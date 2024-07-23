a69518adfc109562ac5fd8efc1ee3bdb79a962e2
Fiono11 <ruipedromorais11@gmail.com>
2024-07-23 15:31:06 +0100

    Remove peers test

--- rust/main/src/cli/commands/ledger/read/peers.rs
+++ rust/main/src/cli/commands/ledger/read/peers.rs
@@ -1,10 +1,7 @@
 use crate::cli::get_path;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::{ArgGroup, Parser};
-use rsnano_store_lmdb::{
-    tests::{Fixture, TEST_PEER_A, TEST_PEER_B},
-    LmdbEnv, LmdbPeerStore,
-};
+use rsnano_store_lmdb::{LmdbEnv, LmdbPeerStore};
 use std::sync::Arc;
 
 #[derive(Parser)]
@@ -17,52 +14,22 @@ pub(crate) struct PeersArgs {
     /// Uses the supplied network (live, test, beta or dev)
     #[arg(long, group = "input")]
     network: Option<String>,
-    #[arg(long)]
-    test: bool,
 }
 
 impl PeersArgs {
     pub(crate) fn peers(&self) -> Result<()> {
-        if self.test {
-            let fixture = Fixture::with_stored_data(vec![TEST_PEER_A, TEST_PEER_B]);
+        let path = get_path(&self.data_path, &self.network).join("data.ldb");
 
-            let mut txn = fixture.env.tx_begin_read();
+        let env = Arc::new(LmdbEnv::new(&path)?);
 
-            for peer in fixture.store.iter(&mut txn) {
-                println!("{:?}", peer);
-            }
-        } else {
-            let path = get_path(&self.data_path, &self.network).join("data.ldb");
+        let peer_store = LmdbPeerStore::new(env.clone())?;
 
-            let env = Arc::new(LmdbEnv::new(&path)?);
+        let mut txn = env.tx_begin_read();
 
-            let peer_store = LmdbPeerStore::new(env.clone())
-                .map_err(|e| anyhow!("Error opening store: {:?}", e))?;
-
-            let mut txn = env.tx_begin_read();
-
-            for peer in peer_store.iter(&mut txn) {
-                println!("{:?}", peer);
-            }
+        for peer in peer_store.iter(&mut txn) {
+            println!("{:?}", peer);
         }
 
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use assert_cmd::Command;
-
-    const PEERS_STR: &str = "([1:2:3:4:5:6:7:8]:1000, SystemTime { tv_sec: 0, tv_nsec: 0 })\n([3:3:3:3:3:3:3:3]:2000, SystemTime { tv_sec: 0, tv_nsec: 0 })\n";
-
-    #[test]
-    fn peers() {
-        Command::cargo_bin("rsnano_node")
-            .unwrap()
-            .args(["ledger", "read", "peers", "--test"])
-            .assert()
-            .success()
-            .stdout(PEERS_STR);
     }
 }

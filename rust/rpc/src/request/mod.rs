a5708694e63e43f3e76e231824859796dd0c6f23
Fiono <ruipedromorais11@gmail.com>
2024-08-12 18:51:58 +0100

    Remove unneeded

--- rust/rpc/src/request/mod.rs
+++ rust/rpc/src/request/mod.rs
@@ -6,7 +6,6 @@ use serde::Deserialize;
 pub(crate) use wallet::*;
 
 #[derive(Deserialize)]
-#[serde(rename_all = "snake_case")]
 #[serde(untagged)]
 pub(crate) enum RpcRequest {
     Node(NodeRpcRequest),

4222107e888a29f7a46c9d482494892cb9523188
Fiono <ruipedromorais11@gmail.com>
2024-08-26 20:07:47 +0100

    WIP

--- rust/rpc_messages/src/dtos/ledger/account_info.rs
+++ rust/rpc_messages/src/dtos/ledger/account_info.rs
@@ -7,15 +7,21 @@ pub struct AccountInfoDto {
     pub open_block: BlockHash,
     pub representative_block: BlockHash,
     pub balance: u128,
+    pub confirmed_balance: Option<u128>,
     pub modified_timestamp: u64,
     pub block_count: u64,
     pub account_version: u8,
-    pub confirmation_height: u64,
-    pub confirmation_height_frontier: BlockHash,
+    pub confirmation_height: Option<u64>,
+    pub confirmation_height_frontier: Option<BlockHash>,
+    pub confirmed_height: Option<u64>,
+    pub confirmed_frontier: Option<BlockHash>,
     pub representative: Option<Account>,
+    pub confirmed_representative: Option<Account>,
     pub weight: Option<u128>,
     pub pending: Option<u128>,
     pub receivable: Option<u128>,
+    pub confirmed_pending: Option<u128>,
+    pub confirmed_receivable: Option<u128>,
 }
 
 impl AccountInfoDto {
@@ -27,12 +33,6 @@ impl AccountInfoDto {
         modified_timestamp: u64,
         block_count: u64,
         account_version: u8,
-        confirmation_height: u64,
-        confirmation_height_frontier: BlockHash,
-        representative: Option<Account>,
-        weight: Option<u128>,
-        pending: Option<u128>,
-        receivable: Option<u128>,
     ) -> Self {
         AccountInfoDto {
             frontier,
@@ -42,12 +42,66 @@ impl AccountInfoDto {
             modified_timestamp,
             block_count,
             account_version,
-            confirmation_height,
-            confirmation_height_frontier,
-            representative,
-            weight,
-            pending,
-            receivable,
+            confirmed_balance: None,
+            confirmation_height: None,
+            confirmation_height_frontier: None,
+            confirmed_height: None,
+            confirmed_frontier: None,
+            representative: None,
+            confirmed_representative: None,
+            weight: None,
+            pending: None,
+            receivable: None,
+            confirmed_pending: None,
+            confirmed_receivable: None,
         }
+    }
+
+    pub fn set_confirmed_balance(&mut self, balance: u128) {
+        self.confirmed_balance = Some(balance);
+    }
+
+    pub fn set_confirmation_height(&mut self, height: u64) {
+        self.confirmation_height = Some(height);
+    }
+
+    pub fn set_confirmation_height_frontier(&mut self, frontier: BlockHash) {
+        self.confirmation_height_frontier = Some(frontier);
+    }
+
+    pub fn set_confirmed_height(&mut self, height: u64) {
+        self.confirmed_height = Some(height);
+    }
+
+    pub fn set_confirmed_frontier(&mut self, frontier: BlockHash) {
+        self.confirmed_frontier = Some(frontier);
+    }
+
+    pub fn set_representative(&mut self, representative: Account) {
+        self.representative = Some(representative);
+    }
+
+    pub fn set_confirmed_representative(&mut self, representative: Account) {
+        self.confirmed_representative = Some(representative);
+    }
+
+    pub fn set_weight(&mut self, weight: u128) {
+        self.weight = Some(weight);
+    }
+
+    pub fn set_pending(&mut self, pending: u128) {
+        self.pending = Some(pending);
+    }
+
+    pub fn set_receivable(&mut self, receivable: u128) {
+        self.receivable = Some(receivable);
+    }
+
+    pub fn set_confirmed_pending(&mut self, pending: u128) {
+        self.confirmed_pending = Some(pending);
+    }
+
+    pub fn set_confirmed_receivable(&mut self, receivable: u128) {
+        self.confirmed_receivable = Some(receivable);
     }
 }
--- rust/rpc_server/src/service/responses/ledger/account_info.rs
+++ rust/rpc_server/src/service/responses/ledger/account_info.rs
@@ -1,4 +1,4 @@
-use rsnano_core::{Account, Amount};
+use rsnano_core::Account;
 use rsnano_node::node::Node;
 use rsnano_rpc_messages::AccountInfoDto;
 use serde_json::to_string_pretty;
@@ -14,11 +14,13 @@ pub async fn account_info(
     include_confirmed: Option<bool>,
 ) -> String {
     let txn = node.ledger.read_txn();
+    let include_confirmed = include_confirmed.unwrap_or(false);
 
     let info = node.ledger.any().get_account(&txn, &account).unwrap();
 
     let confirmation_height_info = node.store.confirmation_height.get(&txn, &account).unwrap();
-    let account_info = AccountInfoDto::new(
+
+    let mut account_info = AccountInfoDto::new(
         info.head,
         info.open_block,
         node.ledger.representative_block_hash(&txn, &info.head),
@@ -26,13 +28,10 @@ pub async fn account_info(
         info.modified,
         info.block_count,
         info.epoch as u8,
-        confirmation_height_info.height,
-        confirmation_height_info.frontier,
-        None,
-        None,
-        None,
-        None,
     );
+
+    account_info.confirmed_height = Some(confirmation_height_info.height);
+    account_info.confirmation_height_frontier = Some(confirmation_height_info.frontier);
 
     to_string_pretty(&account_info).unwrap()
 }

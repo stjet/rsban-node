c3ac292d1bcf5f73e45992aafdaae9226762fc4e
Fiono <ruipedromorais11@gmail.com>
2024-08-26 17:58:46 +0100

    Split rpc responses

--- rust/rpc_server/src/service/responses/mod.rs
+++ rust/rpc_server/src/service/responses/mod.rs
@@ -1,4 +1,13 @@
+mod ledger;
+mod node;
+mod utils;
+mod wallets;
+
+pub use ledger::*;
+pub use node::*;
 use serde_json::{json, to_string_pretty};
+pub use utils::*;
+pub use wallets::*;
 
 fn format_error_message(error: &str) -> String {
     let json_value = json!({ "error": error });

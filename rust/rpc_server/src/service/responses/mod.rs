b8944c37071f03ecaaa441dd38c14e66ffc34206
Fiono <ruipedromorais11@gmail.com>
2024-08-26 15:57:38 +0100

    Add format_error_message function

---
+++ rust/rpc_server/src/service/responses/mod.rs
@@ -0,0 +1,6 @@
+use serde_json::{json, to_string_pretty};
+
+fn format_error_message(error: &str) -> String {
+    let json_value = json!({ "error": error });
+    to_string_pretty(&json_value).unwrap()
+}

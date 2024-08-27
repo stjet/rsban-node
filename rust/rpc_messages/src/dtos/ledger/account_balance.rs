20d0f7ca8dd37d31ac540be8485f81dc0e3d2aa8
Fiono <ruipedromorais11@gmail.com>
2024-08-27 15:36:58 +0100

    Remove unneeded

--- rust/rpc_messages/src/dtos/ledger/account_balance.rs
+++ rust/rpc_messages/src/dtos/ledger/account_balance.rs
@@ -1,7 +1,7 @@
 use rsnano_core::Amount;
-use serde::{ser::SerializeStruct, Deserialize, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Eq, Debug, Deserialize)]
+#[derive(PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub struct AccountBalanceDto {
     pub balance: Amount,
     pub pending: Amount,
@@ -15,18 +15,5 @@ impl AccountBalanceDto {
             pending,
             receivable,
         }
-    }
-}
-
-impl Serialize for AccountBalanceDto {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("AccountBalance", 3)?;
-        state.serialize_field("balance", &self.balance.to_string_dec())?;
-        state.serialize_field("pending", &self.pending.to_string_dec())?;
-        state.serialize_field("receivable", &self.receivable.to_string_dec())?;
-        state.end()
     }
 }

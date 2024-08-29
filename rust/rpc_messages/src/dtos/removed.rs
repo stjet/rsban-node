9a07f22a5859039d7e366218a27b3b01b48fdc1e
Fiono <ruipedromorais11@gmail.com>
2024-08-29 23:32:29 +0100

    Add tests

--- rust/rpc_messages/src/dtos/removed.rs
+++ rust/rpc_messages/src/dtos/removed.rs
@@ -10,3 +10,36 @@ impl RemovedDto {
         Self { removed }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::{from_str, to_string};
+
+    #[test]
+    fn serialize_destroyed_dto() {
+        let dto = RemovedDto::new(true);
+
+        let serialized = to_string(&dto).unwrap();
+
+        let expected_json = serde_json::json!({
+            "removed": true
+        });
+
+        let actual_json: serde_json::Value = from_str(&serialized).unwrap();
+        assert_eq!(actual_json, expected_json);
+    }
+
+    #[test]
+    fn deserialize_destroyed_dto() {
+        let json_str = r#"{
+            "removed": true
+        }"#;
+
+        let deserialized: RemovedDto = from_str(json_str).unwrap();
+
+        let expected = RemovedDto::new(true);
+
+        assert_eq!(deserialized, expected);
+    }
+}

cf5f9856c322ab0b67f335be8c5676fd80c36ca4
Fiono <ruipedromorais11@gmail.com>
2024-08-29 14:47:51 +0100

    Add (de)serialization tests

--- rust/rpc_messages/src/commands/wallets/accounts_create.rs
+++ rust/rpc_messages/src/commands/wallets/accounts_create.rs
@@ -13,3 +13,30 @@ pub struct AccountsCreateArgs {
     pub wallet: WalletId,
     pub count: u64,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::RpcCommand;
+    use rsnano_core::WalletId;
+    use serde_json::to_string_pretty;
+
+    #[test]
+    fn serialize_accounts_create_command() {
+        assert_eq!(
+            to_string_pretty(&RpcCommand::accounts_create(1.into(), 2)).unwrap(),
+            r#"{
+  "action": "accounts_create",
+  "wallet": "0000000000000000000000000000000000000000000000000000000000000001",
+  "count": 2
+}"#
+        )
+    }
+
+    #[test]
+    fn deserialize_accounts_create_command_index_none() {
+        let cmd = RpcCommand::accounts_create(1.into(), 2);
+        let serialized = serde_json::to_string_pretty(&cmd).unwrap();
+        let deserialized: RpcCommand = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(cmd, deserialized)
+    }
+}

b4a809ec7ef375ae4cccef2a54af2df744f3e9f8
Gustav Schauwecker <service@simpago.com>
2024-08-29 11:26:25 +0200

    Partially merge ascending bootstrap dependency resolution

--- rust/ffi/src/config/bootstrap_config.rs
+++ rust/ffi/src/config/bootstrap_config.rs
@@ -3,14 +3,18 @@ use std::time::Duration;
 
 #[repr(C)]
 pub struct BootstrapAscendingConfigDto {
+    pub enable: bool,
+    pub enable_database_scan: bool,
+    pub enable_dependency_walker: bool,
     pub requests_limit: usize,
-    pub database_requests_limit: usize,
+    pub database_rate_limit: usize,
     pub pull_count: usize,
     pub timeout_ms: u64,
     pub throttle_coefficient: usize,
     pub throttle_wait_ms: u64,
     pub account_sets: AccountSetsConfigDto,
     pub block_wait_count: usize,
+    pub max_requests: usize,
 }
 
 #[repr(C)]
@@ -24,14 +28,18 @@ pub struct AccountSetsConfigDto {
 impl From<&BootstrapAscendingConfig> for BootstrapAscendingConfigDto {
     fn from(value: &BootstrapAscendingConfig) -> Self {
         Self {
+            enable: value.enable,
+            enable_database_scan: value.enable_database_scan,
+            enable_dependency_walker: value.enable_dependency_walker,
             requests_limit: value.requests_limit,
-            database_requests_limit: value.database_requests_limit,
+            database_rate_limit: value.database_rate_limit,
             pull_count: value.pull_count,
             timeout_ms: value.request_timeout.as_millis() as u64,
             throttle_coefficient: value.throttle_coefficient,
             throttle_wait_ms: value.throttle_wait.as_millis() as u64,
             account_sets: (&value.account_sets).into(),
             block_wait_count: value.block_wait_count,
+            max_requests: value.max_requests,
         }
     }
 }
@@ -39,14 +47,18 @@ impl From<&BootstrapAscendingConfig> for BootstrapAscendingConfigDto {
 impl From<&BootstrapAscendingConfigDto> for BootstrapAscendingConfig {
     fn from(value: &BootstrapAscendingConfigDto) -> Self {
         let mut config = BootstrapAscendingConfig::default();
+        config.enable = value.enable;
+        config.enable_database_scan = value.enable_database_scan;
+        config.enable_dependency_walker = value.enable_dependency_walker;
         config.requests_limit = value.requests_limit;
-        config.database_requests_limit = value.database_requests_limit;
+        config.database_rate_limit = value.database_rate_limit;
         config.pull_count = value.pull_count;
         config.request_timeout = Duration::from_millis(value.timeout_ms);
         config.throttle_coefficient = value.throttle_coefficient;
         config.throttle_wait = Duration::from_millis(value.throttle_wait_ms);
         config.account_sets = (&value.account_sets).into();
         config.block_wait_count = value.block_wait_count;
+        config.max_requests = value.max_requests;
         config
     }
 }

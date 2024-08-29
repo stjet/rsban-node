112aeb7e5a458c804356dc727db8ebea82673276
Fiono <ruipedromorais11@gmail.com>
2024-08-29 19:44:12 +0100

    More testing

--- rust/rpc_server/src/service/responses/wallets/wallet_balances.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_balances.rs
@@ -4,8 +4,6 @@ use rsnano_rpc_messages::{AccountBalanceDto, WalletBalancesDto};
 use serde_json::to_string_pretty;
 use std::{collections::HashMap, sync::Arc};
 
-use crate::service::responses::format_error_message;
-
 pub async fn wallet_balances(
     node: Arc<Node>,
     wallet: WalletId,
@@ -16,12 +14,12 @@ pub async fn wallet_balances(
     let mut balances = HashMap::new();
     let tx = node.ledger.read_txn();
     for account in accounts {
-        let balance = match node.ledger.confirmed().account_balance(&tx, &account) {
+        let balance = match node.ledger.any().account_balance(&tx, &account) {
             Some(balance) => balance,
-            None => return format_error_message("Account not found"),
+            None => Amount::zero(),
         };
 
-        let pending = node.ledger.account_receivable(&tx, &account, true);
+        let pending = node.ledger.account_receivable(&tx, &account, false);
 
         let account_balance = AccountBalanceDto::new(balance, pending, pending);
         if balance >= threshold {
@@ -34,11 +32,33 @@ pub async fn wallet_balances(
 #[cfg(test)]
 mod tests {
     use crate::service::responses::test_helpers::setup_rpc_client_and_server;
-    use rsnano_core::{Account, Amount};
-    use rsnano_node::wallets::WalletsExt;
+    use rsnano_core::{
+        Account, Amount, BlockEnum, PublicKey, RawKey, StateBlock, WalletId, DEV_GENESIS_KEY,
+    };
+    use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH, DEV_GENESIS_PUB_KEY};
+    use rsnano_node::{node::Node, wallets::WalletsExt};
     use rsnano_rpc_messages::{AccountBalanceDto, WalletBalancesDto};
-    use std::collections::HashMap;
-    use test_helpers::System;
+    use std::{collections::HashMap, sync::Arc, time::Duration};
+    use test_helpers::{assert_timely_msg, System};
+
+    fn send_block(node: Arc<Node>, account: Account) {
+        let send1 = BlockEnum::State(StateBlock::new(
+            *DEV_GENESIS_ACCOUNT,
+            *DEV_GENESIS_HASH,
+            *DEV_GENESIS_PUB_KEY,
+            Amount::MAX - Amount::raw(1),
+            account.into(),
+            &DEV_GENESIS_KEY,
+            node.work_generate_dev((*DEV_GENESIS_HASH).into()),
+        ));
+
+        node.process_active(send1.clone());
+        assert_timely_msg(
+            Duration::from_secs(5),
+            || node.active.active(&send1),
+            "not active on node 1",
+        );
+    }
 
     #[test]
     fn wallet_balances_threshold_none() {
@@ -70,11 +90,58 @@ mod tests {
 
         let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), false);
 
-        node.wallets.create(1.into());
+        let wallet: WalletId = 1.into();
+        let private_key = RawKey::zero();
+        let public_key: PublicKey = (&private_key).try_into().unwrap();
+
+        node.wallets.create(wallet);
+
+        node.wallets
+            .insert_adhoc2(&wallet, &RawKey::zero(), false)
+            .unwrap();
+
+        send_block(node.clone(), public_key.into());
 
         let result = node.tokio.block_on(async {
             rpc_client
-                .wallet_balances(1.into(), Some(Amount::zero()))
+                .wallet_balances(wallet, Some(Amount::zero()))
+                .await
+                .unwrap()
+        });
+
+        let mut expected_balances: HashMap<Account, AccountBalanceDto> = HashMap::new();
+        expected_balances.insert(
+            public_key.into(),
+            AccountBalanceDto::new(Amount::zero(), Amount::raw(1), Amount::raw(1)),
+        );
+        let expected_result = WalletBalancesDto {
+            balances: expected_balances,
+        };
+
+        assert_eq!(result, expected_result);
+
+        server.abort();
+    }
+
+    #[test]
+    fn wallet_balances_threshold_some_fails() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), false);
+
+        node.wallets.create(1.into());
+
+        let public_key = node
+            .wallets
+            .deterministic_insert2(&1.into(), false)
+            .unwrap();
+
+        send_block(node.clone(), public_key.into());
+
+        let result = node.tokio.block_on(async {
+            rpc_client
+                .wallet_balances(1.into(), Some(Amount::raw(2)))
                 .await
                 .unwrap()
         });

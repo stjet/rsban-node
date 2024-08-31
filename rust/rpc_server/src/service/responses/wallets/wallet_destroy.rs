60f7950aa5a912fc00f84ee63977a5d7402bc63b
Fiono <ruipedromorais11@gmail.com>
2024-08-31 12:43:21 +0100

    Refractoring

--- rust/rpc_server/src/service/responses/wallets/wallet_destroy.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_destroy.rs
@@ -1,14 +1,15 @@
-use crate::service::responses::{format_bool_message, format_error_message};
 use rsnano_core::WalletId;
 use rsnano_node::node::Node;
+use rsnano_rpc_messages::{BoolDto, ErrorDto};
+use serde_json::to_string_pretty;
 use std::sync::Arc;
 
 pub async fn wallet_destroy(node: Arc<Node>, enable_control: bool, wallet: WalletId) -> String {
     if enable_control {
         node.wallets.destroy(&wallet);
-        format_bool_message("destroyed", true)
+        to_string_pretty(&BoolDto::new("destroyed".to_string(), true)).unwrap()
     } else {
-        format_error_message("RPC control is disabled")
+        to_string_pretty(&ErrorDto::new("RPC control is disabled".to_string())).unwrap()
     }
 }
 

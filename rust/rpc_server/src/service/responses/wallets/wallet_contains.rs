d7ffd7595a81086084e144e1a9a99718cdc6925d
Fiono <ruipedromorais11@gmail.com>
2024-08-31 14:55:15 +0100

    Refractoring

--- rust/rpc_server/src/service/responses/wallets/wallet_contains.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_contains.rs
@@ -1,18 +1,19 @@
-use crate::service::responses::{format_bool_message, format_error_message};
 use rsnano_core::{Account, WalletId};
 use rsnano_node::node::Node;
+use rsnano_rpc_messages::{BoolDto, ErrorDto};
+use serde_json::to_string_pretty;
 use std::sync::Arc;
 
 pub async fn wallet_contains(node: Arc<Node>, wallet: WalletId, account: Account) -> String {
     let wallet_accounts = match node.wallets.get_accounts_of_wallet(&wallet) {
         Ok(accounts) => accounts,
-        Err(e) => return format_error_message(&e.to_string()),
+        Err(e) => return to_string_pretty(&ErrorDto::new(e.to_string())).unwrap(),
     };
 
     if wallet_accounts.contains(&account) {
-        format_bool_message("exists", true)
+        to_string_pretty(&BoolDto::new("exists".to_string(), true)).unwrap()
     } else {
-        format_bool_message("exists", false)
+        to_string_pretty(&BoolDto::new("exists".to_string(), false)).unwrap()
     }
 }
 

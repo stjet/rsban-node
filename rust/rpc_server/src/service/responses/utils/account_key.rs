5521d1e8612e8d913b102587e0575d91d9aeeeb0
Fiono <ruipedromorais11@gmail.com>
2024-09-01 12:22:02 +0100

    Delete account_key

--- rust/rpc_server/src/service/responses/utils/account_key.rs
+++
@@ -1,6 +0,0 @@
-use rsnano_core::Account;
-use serde_json::to_string_pretty;
-
-pub async fn account_key(account: Account) -> String {
-    to_string_pretty(&AccountKey::new(account.encode_hex())).unwrap()
-}

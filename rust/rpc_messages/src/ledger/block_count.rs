ab1523e50d8d6acdeaf62eb24077a1f90ad3a014
Fiono <ruipedromorais11@gmail.com>
2024-09-02 00:19:11 +0100

    Improvements

--- rust/rpc_messages/src/ledger/block_count.rs
+++ rust/rpc_messages/src/ledger/block_count.rs
@@ -1,6 +1,12 @@
 use crate::RpcCommand;
 use serde::{Deserialize, Serialize};
 
+impl RpcCommand {
+    pub fn block_count() -> Self {
+        Self::BlockCount
+    }
+}
+
 #[derive(PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub struct BlockCountDto {
     count: u64,
@@ -20,8 +26,29 @@ impl BlockCountDto {
 
 #[cfg(test)]
 mod tests {
-    use crate::RpcCommand;
+    use crate::{BlockCountDto, RpcCommand};
     use serde_json::{from_str, to_string_pretty};
+
+    #[test]
+    fn serialize_block_count_dto() {
+        let block_count_dto = BlockCountDto::new(1, 1, 1);
+        assert_eq!(
+            serde_json::to_string_pretty(&block_count_dto).unwrap(),
+            r#"{
+  "count": 1,
+  "unchecked": 1,
+  "cemented": 1
+}"#
+        );
+    }
+
+    #[test]
+    fn deserialize_block_account_dto() {
+        let bool_dto = BlockCountDto::new(1, 1, 1);
+        let serialized = to_string_pretty(&bool_dto).unwrap();
+        let deserialized: BlockCountDto = from_str(&serialized).unwrap();
+        assert_eq!(bool_dto, deserialized);
+    }
 
     #[test]
     fn serialize_account_block_count_command() {

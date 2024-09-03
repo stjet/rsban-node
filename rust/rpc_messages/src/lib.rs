a4834757c7429805f8d912f00e1c538383aaa927
Fiono <ruipedromorais11@gmail.com>
2024-09-03 10:29:42 +0100

    Implement wallet_work_get rpc

--- rust/rpc_messages/src/lib.rs
+++ rust/rpc_messages/src/lib.rs
@@ -22,6 +22,7 @@ pub enum RpcCommand {
     Send(SendArgs),
     WalletAdd(WalletAddArgs),
     WalletCreate,
+    WalletWorkGet(WalletRpcMessage),
 }
 
 #[cfg(test)]
--- rust/rpc_server/src/service/responses/wallets/mod.rs
+++ rust/rpc_server/src/service/responses/wallets/mod.rs
@@ -1,1 +1,3 @@
+mod wallet_work_get;
 
+pub use wallet_work_get::*;

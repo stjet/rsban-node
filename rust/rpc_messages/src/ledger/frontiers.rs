56e8170d4bf798bfb693843cc2923b7b78c9c82b
Fiono <ruipedromorais11@gmail.com>
2024-09-04 18:34:35 +0100

    Fix

--- rust/rpc_messages/src/ledger/frontiers.rs
+++ rust/rpc_messages/src/ledger/frontiers.rs
@@ -14,23 +14,23 @@ mod tests {
     use serde_json::to_string_pretty;
 
     #[test]
-    fn serialize_accounts_frontiers_command() {
+    fn serialize_frontiers_command() {
         assert_eq!(
             to_string_pretty(&RpcCommand::frontiers(Account::zero(), 1)).unwrap(),
             r#"{
   "action": "frontiers",
-  "accounts": [
-    "nano_1111111111111111111111111111111111111111111111111111hifc8npp"
-  ]
+  "account": "nano_1111111111111111111111111111111111111111111111111111hifc8npp",
+  "count": 1
 }"#
         )
     }
 
     #[test]
-    fn deserialize_accounts_frontiers_command() {
+    fn deserialize_frontiers_command() {
         let json_str = r#"{
-    "action": "frontiers",
-    "accounts": ["nano_1111111111111111111111111111111111111111111111111111hifc8npp"]
+  "action": "frontiers",
+  "account": "nano_1111111111111111111111111111111111111111111111111111hifc8npp",
+  "count": 1
     }"#;
         let deserialized: RpcCommand = serde_json::from_str(json_str).unwrap();
         let expected_command = RpcCommand::frontiers(Account::zero(), 1);

b1361ca0aa8d54f8d0c7acbee22173d666317e74
Fiono <ruipedromorais11@gmail.com>
2024-09-16 13:04:44 +0100

    Remove file

--- rust/rpc_messages/src/wallets/mod.rs
+++ rust/rpc_messages/src/wallets/mod.rs
@@ -3,11 +3,9 @@ mod send;
 mod wallet;
 mod wallet_add;
 mod wallet_with_account;
-mod wallet_with_password;
 
 pub use receive::*;
 pub use send::*;
 pub use wallet::*;
 pub use wallet_add::*;
 pub use wallet_with_account::*;
-pub use wallet_with_password::*;

9a6b871317aca6d198932e21a3c67dc16c297b55
Fiono <ruipedromorais11@gmail.com>
2024-09-16 21:03:18 +0100

    WIP

--- rust/rpc_server/src/service/responses/node/work_generate.rs
+++ rust/rpc_server/src/service/responses/node/work_generate.rs
@@ -1,61 +1,123 @@
 use std::sync::Arc;
 use rsnano_node::node::Node;
-use rsnano_rpc_messages::{WorkGenerateArgs, WorkGenerateDto};
-use rsnano_core::WorkVersion;
+use rsnano_rpc_messages::{ErrorDto, WorkGenerateArgs, WorkGenerateDto};
+use rsnano_core::{Account, BlockDetails, BlockEnum, BlockType, DifficultyV1, Epoch, PendingKey, WorkVersion};
+use serde_json::to_string_pretty;
 
-pub async fn work_generate(node: Arc<Node>, enable_control: bool, args: WorkGenerateArgs) -> Result<WorkGenerateDto, String> {
+pub async fn work_generate(node: Arc<Node>, enable_control: bool, args: WorkGenerateArgs) -> String {
+    if !enable_control {
+        return to_string_pretty(&ErrorDto::new("RPC control is disabled".to_string())).unwrap()
+    }
+
     let work_version = args.version.unwrap_or(WorkVersion::Work1);
-    let difficulty = args.difficulty.unwrap(); //_or_else(|| node.default_difficulty(work_version));
+    let default_difficulty = node.ledger.constants.work.threshold_base(work_version);
+    let mut difficulty = args.difficulty.unwrap_or_else(|| default_difficulty);
+
+    let max_difficulty = DifficultyV1::from_multiplier(node.config.max_work_generate_multiplier, default_difficulty);
     
     // Validate difficulty
-    //if difficulty > node.max_work_generate_difficulty(work_version) || 
-       //difficulty < node.network_params.work.threshold_entry(work_version, BlockType::State) {
-        //return Err("Difficulty out of valid range".to_string());
-    //}
+    if difficulty > max_difficulty || 
+       difficulty < node.network_params.work.threshold_entry(BlockType::State, work_version) {
+       return to_string_pretty(&ErrorDto::new("Difficulty out of valid range".to_string())).unwrap()
+    }
 
     // Handle block if provided
     if let Some(block) = args.block {
-        if args.version.is_some() && work_version != WorkVersion::Work1 {
-            return Err("Block work version mismatch".to_string());
+        let block_enum: BlockEnum = block.into();
+        if args.hash != block_enum.hash() {
+            return to_string_pretty(&ErrorDto::new("Block root mismatch".to_string())).unwrap()
+        }
+        if args.version.is_some() && work_version != block_enum.work_version() {
+            return to_string_pretty(&ErrorDto::new("Block work version mismatch".to_string())).unwrap()
         }
         // Recalculate difficulty if not provided
         if args.difficulty.is_none() && args.multiplier.is_none() {
-            //difficulty = node.difficulty_ledger(&block);
+            difficulty = difficulty_ledger(node.clone(), &block_enum.into());
         }
         if node.network_params.work.difficulty(work_version, &args.hash.into(), 0) >= difficulty {
-            return Err("Block work is already sufficient".to_string());
+            return to_string_pretty(&ErrorDto::new("Block work is already sufficient".to_string())).unwrap()
         }
     }
 
     let use_peers = args.use_peers.unwrap_or(false);
-    //let account = args.account.unwrap();
-    //args.account.or_else(|| {
+    let mut account = args.account;
+    if account.is_none() {
         // Fetch account from block if not given
-        //node.ledger.block_account(&args.hash)
-    //});
+        account = node.ledger.any().block_account(&node.ledger.read_txn(), &args.hash);
+    }
+
+    //let secondary_work_peers = args.secondary_work_peers.unwrap_or(false);
 
     let work_result = if !use_peers {
-        //if node.local_work_generation_enabled() {
-            node.distributed_work.generate_in_local_work_pool(args.hash.into(), difficulty).await
-        //} else {
-            //return Err("Local work generation is disabled".to_string());
-        //}
+        if node.distributed_work.work_generation_enabled() {
+            node.distributed_work.make(args.hash.into(),difficulty, account).await
+        } else {
+            return to_string_pretty(&ErrorDto::new("Local work generation is disabled".to_string())).unwrap()
+        }
     } else {
         if node.distributed_work.work_generation_enabled() {
-            node.distributed_work.make(args.hash.into(), difficulty, args.account).await
-
+            node.distributed_work.make(args.hash.into(),difficulty, account).await
         } else {
-            return Err("Work generation is disabled".to_string());
+            return to_string_pretty(&ErrorDto::new("Work generation is disabled".to_string())).unwrap()
         }
     };
 
     let result_difficulty = node.network_params.work.difficulty(work_version, &args.hash.into(), work_result.unwrap());
-    //let result_multiplier = node.difficulty_to_multiplier(result_difficulty, node.default_difficulty(work_version));
+    let result_multiplier = DifficultyV1::to_multiplier(result_difficulty, node.ledger.constants.work.threshold_base(work_version));
 
-    Ok(WorkGenerateDto::new(
+    let work_generate_dto = WorkGenerateDto::new(
         work_result.unwrap().into(),
         result_difficulty,
-        Some(0.),
+        Some(result_multiplier),
         args.hash,
-    ))
+    );
+
+    to_string_pretty(&work_generate_dto).unwrap()
+}
+
+fn difficulty_ledger(node: Arc<Node>, block: &BlockEnum) -> u64 {
+    let mut details = BlockDetails::new(Epoch::Epoch0, false, false, false);
+    let mut details_found = false;
+
+    let transaction = node.store.tx_begin_read();
+    
+    // Previous block find
+    let mut block_previous: Option<BlockEnum> = None;
+    let previous = block.previous();
+    if !previous.is_zero() {
+        block_previous = node.ledger.any().get_block(&transaction, &previous);
+    }
+
+    // Send check
+    if let Some(prev_block) = &block_previous {
+        let is_send = node.ledger.any().block_balance(&transaction, &previous) > block.balance_field();
+        details = BlockDetails::new(Epoch::Epoch0, is_send, false, false);
+        details_found = true;
+    }
+
+    // Epoch check
+    if let Some(prev_block) = &block_previous {
+        let epoch = prev_block.sideband().unwrap().details.epoch;
+        details = BlockDetails::new(epoch, details.is_send, details.is_receive, details.is_epoch);
+    }
+
+    // Link check
+    if let Some(link) = block.link_field() {
+        if !details.is_send {
+            if let Some(block_link) = node.ledger.any().get_block(&transaction, &link.into()) {
+                let account = block.account_field().unwrap();
+                if node.ledger.any().get_pending(&transaction, &PendingKey::new(account, link.into())).is_some() {
+                    let epoch = std::cmp::max(details.epoch, block_link.sideband().unwrap().details.epoch);
+                    details = BlockDetails::new(epoch, details.is_send, true, details.is_epoch);
+                    details_found = true;
+                }
+            }
+        }
+    }
+
+    if details_found {
+        node.network_params.work.threshold(&details)
+    } else {
+        node.network_params.work.threshold_base(block.work_version())
+    }
 }

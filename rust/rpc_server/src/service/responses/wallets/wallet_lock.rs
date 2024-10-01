f1484e8dff36cce605d1114329fe3abb545b7e7f
Fiono <ruipedromorais11@gmail.com>
2024-10-01 19:47:52 +0100

    Add test

--- rust/rpc_server/src/service/responses/wallets/wallet_lock.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_lock.rs
@@ -60,9 +60,31 @@ mod tests {
             .tokio
             .block_on(async { rpc_client.wallet_lock(wallet_id).await });
 
-        assert!(result.is_err());
+        assert_eq!(
+            result.err().map(|e| e.to_string()),
+            Some("node returned error: \"RPC control is disabled\"".to_string())
+        );
 
         assert_eq!(node.wallets.valid_password(&wallet_id).unwrap(), true);
+
+        server.abort();
+    }
+
+    #[test]
+    fn wallet_lock_fails_with_wallet_not_found() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
+
+        let result = node
+            .tokio
+            .block_on(async { rpc_client.wallet_lock(WalletId::zero()).await });
+
+        assert_eq!(
+            result.err().map(|e| e.to_string()),
+            Some("node returned error: \"Wallet not found\"".to_string())
+        );
 
         server.abort();
     }

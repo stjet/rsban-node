bb1ca7a0584f55c869a125618e33e1f1660215a3
Fiono <ruipedromorais11@gmail.com>
2024-10-02 12:48:02 +0100

    Improvements

--- rust/rpc_server/src/service/responses/wallets/wallet_representative.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_representative.rs
@@ -43,4 +43,23 @@ mod tests {
 
         server.abort();
     }
+
+    #[test]
+    fn wallet_representative_fails_with_wallet_not_found() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
+
+        let result = node
+            .tokio
+            .block_on(async { rpc_client.wallet_representative(WalletId::zero()).await });
+
+        assert_eq!(
+            result.err().map(|e| e.to_string()),
+            Some("node returned error: \"Wallet not found\"".to_string())
+        );
+
+        server.abort();
+    }
 }

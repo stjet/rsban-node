890d60857935937f96ffea10808c8a7fccb2e619
Fiono <ruipedromorais11@gmail.com>
2024-10-02 13:05:55 +0100

    Improvements

--- rust/rpc_server/src/service/responses/ledger/accounts_frontiers.rs
+++ rust/rpc_server/src/service/responses/ledger/accounts_frontiers.rs
@@ -28,11 +28,12 @@ pub async fn accounts_frontiers(node: Arc<Node>, accounts: Vec<Account>) -> Stri
 #[cfg(test)]
 mod tests {
     use crate::service::responses::test_helpers::setup_rpc_client_and_server;
+    use rsnano_core::Account;
     use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH};
     use test_helpers::System;
 
     #[test]
-    fn accounts_frontiers() {
+    fn accounts_frontiers_found() {
         let mut system = System::new();
         let node = system.make_node();
 
@@ -49,6 +50,58 @@ mod tests {
             result.frontiers.get(&*DEV_GENESIS_ACCOUNT).unwrap(),
             &*DEV_GENESIS_HASH
         );
+
+        server.abort();
+    }
+
+    #[test]
+    fn accounts_frontiers_account_not_found() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
+
+        let result = node.tokio.block_on(async {
+            rpc_client
+                .accounts_frontiers(vec![Account::zero()])
+                .await
+                .unwrap()
+        });
+
+        assert_eq!(
+            result.errors.unwrap().get(&Account::zero()).unwrap(),
+            "Account not found"
+        );
+
+        server.abort();
+    }
+
+    #[test]
+    fn accounts_frontiers_found_and_not_found() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
+
+        let result = node.tokio.block_on(async {
+            rpc_client
+                .accounts_frontiers(vec![*DEV_GENESIS_ACCOUNT, Account::zero()])
+                .await
+                .unwrap()
+        });
+
+        assert_eq!(
+            result.frontiers.get(&*DEV_GENESIS_ACCOUNT).unwrap(),
+            &*DEV_GENESIS_HASH
+        );
+
+        assert_eq!(
+            result.errors.as_ref().unwrap().get(&Account::zero()).unwrap(),
+            "Account not found"
+        );
+
+        assert_eq!(result.frontiers.len(), 1);
+        assert_eq!(result.errors.as_ref().unwrap().len(), 1);
 
         server.abort();
     }

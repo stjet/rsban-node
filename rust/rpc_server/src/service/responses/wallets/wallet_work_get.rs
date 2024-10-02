5b4a56e8787f6cccc98bdc0c60a2217528a7cc0a
Fiono <ruipedromorais11@gmail.com>
2024-10-02 13:00:27 +0100

    Improvements

--- rust/rpc_server/src/service/responses/wallets/wallet_work_get.rs
+++ rust/rpc_server/src/service/responses/wallets/wallet_work_get.rs
@@ -5,27 +5,27 @@ use serde_json::to_string_pretty;
 use std::{collections::HashMap, sync::Arc};
 
 pub async fn wallet_work_get(node: Arc<Node>, enable_control: bool, wallet: WalletId) -> String {
-    if enable_control {
-        let accounts = match node.wallets.get_accounts_of_wallet(&wallet) {
-            Ok(accounts) => accounts,
-            Err(e) => return to_string_pretty(&ErrorDto::new(e.to_string())).unwrap(),
-        };
-
-        let mut works = HashMap::new();
-
-        for account in accounts {
-            match node.wallets.work_get2(&wallet, &account.into()) {
-                Ok(work) => {
-                    works.insert(account, work.into());
-                }
-                Err(e) => return to_string_pretty(&ErrorDto::new(e.to_string())).unwrap(),
-            }
-        }
-
-        to_string_pretty(&AccountsWithWorkDto::new(works)).unwrap()
-    } else {
-        to_string_pretty(&ErrorDto::new("RPC control is disabled".to_string())).unwrap()
+    if !enable_control {
+        return to_string_pretty(&ErrorDto::new("RPC control is disabled".to_string())).unwrap()
     }
+
+    let accounts = match node.wallets.get_accounts_of_wallet(&wallet) {
+        Ok(accounts) => accounts,
+        Err(e) => return to_string_pretty(&ErrorDto::new(e.to_string())).unwrap(),
+    };
+
+    let mut works = HashMap::new();
+
+    for account in accounts {
+        match node.wallets.work_get2(&wallet, &account.into()) {
+            Ok(work) => {
+                works.insert(account, work.into());
+            }
+            Err(e) => return to_string_pretty(&ErrorDto::new(e.to_string())).unwrap(),
+        }
+    }
+
+    to_string_pretty(&AccountsWithWorkDto::new(works)).unwrap()
 }
 
 #[cfg(test)]
@@ -80,6 +80,25 @@ mod tests {
         assert_eq!(
             result.err().map(|e| e.to_string()),
             Some("node returned error: \"RPC control is disabled\"".to_string())
+        );
+
+        server.abort();
+    }
+
+    #[test]
+    fn wallet_work_get_fails_with_wallet_not_found() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
+
+        let result = node
+            .tokio
+            .block_on(async { rpc_client.wallet_work_get(WalletId::zero()).await });
+
+        assert_eq!(
+            result.err().map(|e| e.to_string()),
+            Some("node returned error: \"Wallet not found\"".to_string())
         );
 
         server.abort();

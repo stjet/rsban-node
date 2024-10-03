1433d1b6013ffaafbe30281c1ae1138a0863cbe3
Fiono <ruipedromorais11@gmail.com>
2024-10-03 12:36:15 +0100

    Improvements

--- rust/rpc_server/src/service/responses/node/sign.rs
+++ rust/rpc_server/src/service/responses/node/sign.rs
@@ -35,9 +35,11 @@ pub async fn sign(node: Arc<Node>, args: SignArgs) -> String {
 #[cfg(test)]
 mod tests {
     use crate::service::responses::test_helpers::setup_rpc_client_and_server;
-    use rsnano_core::{Amount, BlockEnum, StateBlock, DEV_GENESIS_KEY};
+    use rsnano_core::{validate_block_signature, Account, Amount, BlockEnum, StateBlock, WalletId, DEV_GENESIS_KEY};
     use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH, DEV_GENESIS_PUB_KEY};
+    use rsnano_node::wallets::WalletsExt;
     use test_helpers::System;
+    use rsnano_core::PublicKey;
 
     #[test]
     fn sign() {
@@ -46,22 +48,41 @@ mod tests {
 
         let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), false);
 
-        let send1 = BlockEnum::State(StateBlock::new(
+        let key = rsnano_core::KeyPair::new();
+
+        let wallet_id = WalletId::zero();
+        node.wallets.create(wallet_id);
+        node.wallets.insert_adhoc2(&wallet_id, &key.private_key(), false);
+
+        let send = BlockEnum::State(StateBlock::new(
             *DEV_GENESIS_ACCOUNT,
             *DEV_GENESIS_HASH,
             *DEV_GENESIS_PUB_KEY,
-            Amount::MAX - Amount::raw(1),
-            DEV_GENESIS_KEY.account().into(),
+            Amount::MAX - Amount::raw(1_000_000), // Equivalent to nano::Gxrb_ratio
+            Account::from(key.public_key()).into(),
             &DEV_GENESIS_KEY,
             node.work_generate_dev((*DEV_GENESIS_HASH).into()),
         ));
 
         let result = node.tokio.block_on(async {
             rpc_client
-                .sign(Some(DEV_GENESIS_KEY.private_key()), None, None, send1.json_representation())
+                .sign(None, Some(wallet_id), Some(key.public_key().into()), send.json_representation())
                 .await
                 .unwrap()
         });
+
+        let signed_block: BlockEnum = result.block.into();
+
+        if let BlockEnum::State(ref state_block) = signed_block {
+            assert!(validate_block_signature(&state_block).is_ok());
+
+        } else {
+            panic!("Expected a state block");
+        }
+        
+        assert_eq!(signed_block.block_signature(), send.block_signature());
+        
+        assert_eq!(signed_block.hash(), send.hash());
 
         server.abort();
     }

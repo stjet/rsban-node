035931c1edb585667d66cdc9713bd17cc0602f06
Fiono <ruipedromorais11@gmail.com>
2024-10-03 16:17:39 +0100

    Improvements

--- rust/rpc_server/src/service/responses/wallets/receive.rs
+++ rust/rpc_server/src/service/responses/wallets/receive.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
-use rsnano_core::{BlockEnum, BlockHash, PendingKey, StateBlock, WorkVersion};
-use rsnano_node::{node::Node, wallets::{self, WalletsExt}, work::WorkRequest};
+use rsnano_core::PendingKey;
+use rsnano_node::{node::Node, wallets::WalletsExt};
 use rsnano_rpc_messages::{BlockHashRpcMessage, ErrorDto, ReceiveArgs};
 use serde_json::to_string_pretty;
 
@@ -35,8 +35,7 @@ pub async fn receive(node: Arc<Node>, enable_control: bool, args: ReceiveArgs) -
 
     match receive {
         Ok(_) => to_string_pretty(&BlockHashRpcMessage::new("block".to_string(), block.hash())).unwrap(),
-        //Ok(None) => to_string_pretty(&ErrorDto::new("Failed to create receive block".to_string())).unwrap(),
-        Err(e) => to_string_pretty(&ErrorDto::new("Receive error".to_string())).unwrap(),
+        Err(_) => to_string_pretty(&ErrorDto::new("Receive error".to_string())).unwrap(),
     }
 }
 
@@ -46,52 +45,48 @@ mod tests {
     use rsnano_core::{Amount, BlockHash, WalletId, DEV_GENESIS_KEY};
     use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH};
     use rsnano_node::wallets::WalletsExt;
-    use test_helpers::{assert_timely_msg, establish_tcp, System};
+    use test_helpers::{assert_timely_msg, System};
     use crate::service::responses::test_helpers::setup_rpc_client_and_server;
 
     #[test]
     fn receive() {
         let mut system = System::new();
-        let node1 = system.make_node();
-        let node2 = system.make_node();
-        establish_tcp(&node1, &node2);
+        let node = system.make_node();
 
         let wallet = WalletId::zero();
-        node1.wallets.create(wallet);
-        node1.wallets.insert_adhoc2(&wallet, &DEV_GENESIS_KEY.private_key(), false).unwrap();
+        node.wallets.create(wallet);
+        node.wallets.insert_adhoc2(&wallet, &DEV_GENESIS_KEY.private_key(), false).unwrap();
 
-        let wallet2 = WalletId::random();
-        node2.wallets.create(wallet2);
         let key1 = rsnano_core::KeyPair::new();
-        node2.wallets.insert_adhoc2(&wallet2, &key1.private_key(), false).unwrap();
+        node.wallets.insert_adhoc2(&wallet, &key1.private_key(), false).unwrap();
 
-        let (rpc_client, server) = setup_rpc_client_and_server(node2.clone(), true);
+        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
 
         // Send first block (above minimum receive amount)
-        let send1 = node1.wallets.send_action2(
+        let send1 = node.wallets.send_action2(
             &wallet,
             *DEV_GENESIS_ACCOUNT,
             key1.public_key().into(),
-            node1.config.receive_minimum,
-            node1.work_generate_dev((*DEV_GENESIS_HASH).into()),
+            node.config.receive_minimum,
+            node.work_generate_dev((*DEV_GENESIS_HASH).into()),
             true,
             None
         ).unwrap();
 
         assert_timely_msg(
             Duration::from_secs(5),
-            || node1.ledger.any().account_balance(&node1.ledger.read_txn(), &(*DEV_GENESIS_ACCOUNT)) != Some(Amount::MAX),
+            || node.ledger.any().account_balance(&node.ledger.read_txn(), &(*DEV_GENESIS_ACCOUNT)) != Some(Amount::MAX),
             "Genesis account balance not updated",
         );
 
         assert_timely_msg(
             Duration::from_secs(10),
-            || !node1.ledger.any().get_account(&node1.ledger.read_txn(), &key1.public_key().into()).is_some(),
+            || !node.ledger.any().get_account(&node.ledger.read_txn(), &key1.public_key().into()).is_some(),
             "Destination account should not exist yet",
         );
 
         // Send second block (below minimum receive amount)
-        /*let send2 = node.wallets.send_action2(
+        let send2 = node.wallets.send_action2(
             &wallet,
             *DEV_GENESIS_ACCOUNT,
             key1.public_key().into(),
@@ -99,30 +94,28 @@ mod tests {
             node.work_generate_dev(send1.hash().into()),
             true,
             None
-        ).unwrap();*/
+        ).unwrap();
 
         // Receive the second block
-        let block_hash = node2.tokio.block_on(async move {
+        let block_hash = node.tokio.block_on(async {
             rpc_client
                 .receive(
-                    wallet2,
+                    wallet,
                     key1.public_key().into(),
-                    send1.hash(),
+                    send2.hash(),
                     None,
                 )
                 .await
                 .unwrap()
         }).value;
 
-        println!("{:?}", block_hash);
-
         // Verify that the receive transaction was processed
-        /*let tx = node2.ledger.read_txn();
+        let tx = node.ledger.read_txn();
         assert_timely_msg(
             Duration::from_secs(5),
             || {
-                node2.ledger
-                    .get_block(&tx, &receive_result)
+                node.ledger
+                    .get_block(&tx, &block_hash)
                     .is_some()
             },
             "Receive block not found in ledger",
@@ -130,28 +123,30 @@ mod tests {
 
         // Verify the balance of the receiving account
         assert_eq!(
-            node2.ledger.any().account_balance(&tx, &key1.public_key().into()).unwrap(),
-            node2.config.receive_minimum - Amount::raw(1)
+            node.ledger.any().account_balance(&tx, &key1.public_key().into()).unwrap(),
+            node.config.receive_minimum - Amount::raw(1)
         );
 
         // Try to receive the same block again (should fail)
-        let error_result = node2.tokio.block_on(async {
+        let error_result = node.tokio.block_on(async {
             rpc_client
                 .receive(
                     wallet,
                     key1.public_key().into(),
-                    send1.hash(),
+                    send2.hash(),
                     None,
                 )
                 .await
         });
 
-        assert!(error_result.is_err());
-        assert_eq!(error_result.unwrap_err().to_string(), "Block is not receivable");
+        assert_eq!(
+            error_result.err().map(|e| e.to_string()),
+            Some("node returned error: \"Block is not receivable\"".to_string())
+        );
 
         // Try to receive a non-existing block (should fail)
         let non_existing_hash = BlockHash::zero();
-        let error_result = node2.tokio.block_on(async {
+        let error_result = node.tokio.block_on(async {
             rpc_client
                 .receive(
                     wallet,
@@ -162,8 +157,10 @@ mod tests {
                 .await
         });
 
-        assert!(error_result.is_err());
-        assert_eq!(error_result.unwrap_err().to_string(), "Block not found");*/
+        assert_eq!(
+            error_result.err().map(|e| e.to_string()),
+            Some("node returned error: \"Block not found\"".to_string())
+        );
 
         server.abort();
     }

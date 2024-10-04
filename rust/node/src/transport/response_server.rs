48694794e17df35200f60b25e59e556acbbd19ba
Gustav Schauwecker <service@simpago.com>
2024-10-04 16:10:07 +0200

    Change log lever for noisy log message

--- rust/node/src/transport/response_server.rs
+++ rust/node/src/transport/response_server.rs
@@ -375,7 +375,7 @@ impl ResponseServerExt for Arc<ResponseServer> {
                     // IO error or critical error when deserializing message
                     self.stats
                         .inc_dir(StatType::Error, DetailType::from(&e), Direction::In);
-                    info!(
+                    debug!(
                         "Error reading message: {:?} ({})",
                         e,
                         self.remote_endpoint()

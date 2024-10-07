fcb6256f175aaa8ccaf0c818b3e375a9108c1ced
Fiono <ruipedromorais11@gmail.com>
2024-10-07 16:29:45 +0100

    Remove work_peers rpc response implementation

--- rust/rpc_server/src/service/responses/node/work_peers.rs
+++ rust/rpc_server/src/service/responses/node/work_peers.rs
@@ -1,57 +1,6 @@
 use std::sync::Arc;
 use rsnano_node::node::Node;
-use rsnano_rpc_messages::{ErrorDto, WorkPeersDto};
-use serde_json::to_string_pretty;
 
 pub async fn work_peers(node: Arc<Node>, enable_control: bool) -> String {
-    if enable_control {
-        let work_peers_dto = WorkPeersDto::new(node.config.lock().unwrap().work_peers.clone());
-        to_string_pretty(&work_peers_dto).unwrap()
-    }
-    else {
-        to_string_pretty(&ErrorDto::new("RPC control is disabled".to_string())).unwrap()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::net::Ipv6Addr;
-    use crate::service::responses::test_helpers::setup_rpc_client_and_server;
-    use rsnano_node::config::Peer;
-    use test_helpers::System;
-
-    #[test]
-    fn work_peers() {
-        let mut system = System::new();
-        let node = system.make_node();
-
-        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), true);
-
-        node.config.lock().unwrap().work_peers.push(Peer::new(Ipv6Addr::LOCALHOST.to_string(), 54000));
-
-        let result = node
-            .tokio
-            .block_on(async { rpc_client.work_peers().await.unwrap() });
-
-        assert_eq!(result.work_peers[0], Peer::new(Ipv6Addr::LOCALHOST.to_string(), 54000));
-        assert_eq!(result.work_peers.len(), 1);
-
-        server.abort();
-    }
-
-    #[test]
-    fn work_peers_fails_without_enable_control() {
-        let mut system = System::new();
-        let node = system.make_node();
-
-        let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), false);
-
-        let result = node
-            .tokio
-            .block_on(async { rpc_client.work_peers().await });
-
-        assert!(result.is_err());
-
-        server.abort();
-    }
+    todo!("Distributed work feature is not implemented yet")
 }

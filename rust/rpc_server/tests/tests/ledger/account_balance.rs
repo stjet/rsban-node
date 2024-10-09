f3a45555bfe744677c0adad090852742b8aa7545
Fiono <ruipedromorais11@gmail.com>
2024-10-09 23:20:33 +0100

    Undo changes

--- rust/rpc_server/tests/tests/ledger/account_balance.rs
+++ rust/rpc_server/tests/tests/ledger/account_balance.rs
@@ -1,25 +1,22 @@
 use rsnano_core::{Amount, DEV_GENESIS_KEY};
 use rsnano_node::NodeExt;
-use test_helpers::{
-    get_available_port, send_block, setup_rpc_client, setup_rpc_client_and_server,
-    setup_rpc_server, System, TestNode,
-};
+use test_helpers::{send_block, setup_rpc_client_and_server, System};
 
-#[tokio::test]
-async fn account_balance_only_confirmed_none() {
-    let node = TestNode::new().await;
-    node.start();
+#[test]
+fn account_balance_only_confirmed_none() {
+    let mut system = System::new();
+    let node = system.make_node();
 
     send_block(node.clone());
 
-    let port = get_available_port();
-    setup_rpc_server(port, node.clone(), false).await;
-    let rpc_client = setup_rpc_client(port);
+    let (rpc_client, server) = setup_rpc_client_and_server(node.clone(), false);
 
-    let result = rpc_client
-        .account_balance(DEV_GENESIS_KEY.public_key().as_account(), None)
-        .await
-        .unwrap();
+    let result = node.runtime.block_on(async {
+        rpc_client
+            .account_balance(DEV_GENESIS_KEY.public_key().as_account(), None)
+            .await
+            .unwrap()
+    });
 
     assert_eq!(
         result.balance,
@@ -29,6 +26,8 @@ async fn account_balance_only_confirmed_none() {
     assert_eq!(result.pending, Amount::zero());
 
     assert_eq!(result.receivable, Amount::zero());
+
+    server.abort();
 }
 
 #[test]

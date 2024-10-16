2302554c492a58ad5bcd5a3b1529eebc9235a3dc
Fiono <ruipedromorais11@gmail.com>
2024-10-16 21:53:51 +0100

    Port election_scheduler tests WIP

--- rust/node/tests/tests/election_scheduler.rs
+++ rust/node/tests/tests/election_scheduler.rs
@@ -123,3 +123,166 @@ mod bucket {
         assert_eq!(blocks[1], block0);
     }
 }
+
+mod election_scheduler {
+    use std::time::Duration;
+
+    use rsnano_core::{Amount, BlockEnum, BlockHash, KeyPair, StateBlock, DEV_GENESIS_KEY};
+    use rsnano_ledger::{BlockStatus, DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH, DEV_GENESIS_PUB_KEY};
+    use rsnano_node::{config::{FrontiersConfirmationMode, NodeConfig}, consensus::{ActiveElectionsConfig, ActiveElectionsExt, ElectionStatus}, wallets::WalletsExt};
+    use test_helpers::{assert_timely, assert_timely_eq, System};
+
+    use crate::tests::active_elections;
+
+    #[test]
+    fn activate_one_timely() {
+        let mut system = System::new();
+        let config = NodeConfig {
+            frontiers_confirmation: FrontiersConfirmationMode::Disabled,
+            ..System::default_config()
+        };
+        let node = system.build_node().config(config).finish();
+        let wallet_id = node.wallets.wallet_ids()[0];
+        node.wallets
+            .insert_adhoc2(&wallet_id, &DEV_GENESIS_KEY.private_key(), true)
+            .unwrap();
+
+        // Create a send block
+        let send1 = BlockEnum::State(StateBlock::new(
+            *DEV_GENESIS_ACCOUNT,
+            *DEV_GENESIS_HASH,
+            *DEV_GENESIS_PUB_KEY,
+            node.balance(&*DEV_GENESIS_ACCOUNT) - Amount::raw(1_000_000_000_000_000_000_000_000),
+            (*DEV_GENESIS_ACCOUNT).into(),
+            &DEV_GENESIS_KEY,
+            node.work_generate_dev((*DEV_GENESIS_HASH).into()),
+        ));
+
+        // Process the block
+        node.process_active(send1.clone());
+        assert_timely(Duration::from_secs(5), || {
+            node.block(&send1.hash()).is_some()
+        });
+
+        // Activate the account
+        node.election_schedulers.priority.activate(&node.store.tx_begin_read(), &*DEV_GENESIS_ACCOUNT);
+
+        // Assert that the election is created within 5 seconds
+        assert_timely(Duration::from_secs(5), || {
+            node.active.election(&send1.qualified_root()).is_some()
+        });
+    }
+
+    #[test]
+    fn activate_one_flush() {
+        let mut system = System::new();
+        let node = system.make_node();
+
+        // Create a send block
+        let mut send1 = BlockEnum::State(StateBlock::new(
+            *DEV_GENESIS_ACCOUNT,
+            *DEV_GENESIS_HASH,
+            *DEV_GENESIS_PUB_KEY,
+            node.balance(&*DEV_GENESIS_ACCOUNT) - Amount::raw(1_000_000_000_000_000_000_000_000),
+            (*DEV_GENESIS_ACCOUNT).into(),
+            &DEV_GENESIS_KEY,
+            node.work_generate_dev((*DEV_GENESIS_HASH).into()),
+        ));
+
+        // Process the block
+        node.ledger.process(&mut node.store.tx_begin_write(), &mut send1).unwrap();
+
+        // Activate the account
+        node.election_schedulers.priority.activate(&node.store.tx_begin_read(), &*DEV_GENESIS_ACCOUNT);
+
+        // Assert that the election is created within 5 seconds
+        assert_timely(Duration::from_secs(5), || {
+            node.active.election(&send1.qualified_root()).is_some()
+        });
+    }
+
+    #[test]
+    fn no_vacancy() {
+        let mut system = System::new();
+        let mut active_elections = ActiveElectionsConfig::default();
+        active_elections.size = 1;
+        let config = NodeConfig {
+            active_elections,
+            frontiers_confirmation: FrontiersConfirmationMode::Disabled,
+            ..System::default_config()
+        };
+        let node = system.build_node().config(config).finish();
+
+        let key = KeyPair::new();
+
+        // Prepare 2 accounts
+        let send = BlockEnum::State(StateBlock::new(
+            *DEV_GENESIS_ACCOUNT,
+            *DEV_GENESIS_HASH,
+            *DEV_GENESIS_PUB_KEY,
+            node.balance(&*DEV_GENESIS_ACCOUNT) - Amount::raw(1_000_000_000_000_000_000_000_000),
+            key.account().into(),
+            &DEV_GENESIS_KEY,
+            node.work_generate_dev((*DEV_GENESIS_HASH).into()),
+        ));
+        assert_eq!(node.process_local(send.clone()).unwrap(), BlockStatus::Progress);
+        //node.process_confirmed(ElectionStatus::new(send.clone()));
+
+        let receive = BlockEnum::State(StateBlock::new(
+            key.account(),
+            BlockHash::zero(),
+            key.public_key(),
+            Amount::raw(1_000_000_000_000_000_000_000_000),
+            send.hash().into(),
+            &key,
+            node.work_generate_dev(key.public_key().into()),
+        ));
+        assert_eq!(node.process_local(receive.clone()).unwrap(), BlockStatus::Progress);
+        //node.process_confirmed(ElectionStatus::new(receive.clone()));
+        node.confirm(receive.hash());
+
+        assert_timely(Duration::from_secs(5), || { node.confirm_multi(&[send.clone(), receive.clone()]); true });
+
+        // Process two eligible transactions
+        let block1 = BlockEnum::State(StateBlock::new(
+            *DEV_GENESIS_ACCOUNT,
+            send.hash(),
+            *DEV_GENESIS_PUB_KEY,
+            node.balance(&*DEV_GENESIS_ACCOUNT) - Amount::raw(2_000_000_000_000_000_000_000_000),
+            (*DEV_GENESIS_ACCOUNT).into(),
+            &DEV_GENESIS_KEY,
+            node.work_generate_dev(send.hash().into()),
+        ));
+        assert_eq!(node.process_local(block1.clone()).unwrap(), BlockStatus::Progress);
+
+        // There is vacancy so it should be inserted
+        node.election_schedulers.priority.activate(&node.store.tx_begin_read(), &*DEV_GENESIS_ACCOUNT);
+        assert_timely(Duration::from_secs(5), || {
+            node.active.election(&block1.qualified_root()).is_some()
+        });
+
+        let block2 = BlockEnum::State(StateBlock::new(
+            key.account(),
+            receive.hash(),
+            key.public_key(),
+            Amount::zero(),
+            key.account().into(),
+            &key,
+            node.work_generate_dev(receive.hash().into()),
+        ));
+        assert_eq!(node.process_local(block2.clone()).unwrap(), BlockStatus::Progress);
+
+        // There is no vacancy so it should stay queued
+        node.election_schedulers.priority.activate(&node.store.tx_begin_read(), &key.account());
+        assert_timely_eq(Duration::from_secs(5), || node.election_schedulers.priority.len(), 1);
+        assert!(node.active.election(&block2.qualified_root()).is_none());
+
+        // Election confirmed, next in queue should begin
+        let election = node.active.election(&block1.qualified_root()).unwrap();
+        node.active.force_confirm(&election);
+        assert_timely(Duration::from_secs(5), || {
+            node.active.election(&block2.qualified_root()).is_some()
+        });
+        assert!(node.election_schedulers.priority.len() == 0);
+    }
+}

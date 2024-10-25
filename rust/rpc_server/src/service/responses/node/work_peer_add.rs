50612cf2f3bd6b0dc4e432659b76f76aae42a437
Gustav Schauwecker <service@simpago.com>
2024-10-25 07:46:12 +0200

    Don't panic on work_peer_add

--- rust/rpc_server/src/service/responses/node/work_peer_add.rs
+++ rust/rpc_server/src/service/responses/node/work_peer_add.rs
@@ -1,11 +1,13 @@
 use rsnano_node::Node;
-use rsnano_rpc_messages::{AddressWithPortArgs, RpcDto};
+use rsnano_rpc_messages::{AddressWithPortArgs, ErrorDto, RpcDto};
 use std::sync::Arc;
+use tracing::warn;
 
 pub async fn work_peer_add(
     _node: Arc<Node>,
     _enable_control: bool,
     _args: AddressWithPortArgs,
 ) -> RpcDto {
-    todo!("Distributed work feature is not implemented yet")
+    warn!("Distributed work feature is not implemented yet");
+    RpcDto::Error(ErrorDto::Other)
 }

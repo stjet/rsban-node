210ff08fb43c049fb3e81e47dca33f748a29031e
Gustav Schauwecker <service@simpago.com>
2024-11-14 15:51:58 +0100

    Port test `bootstrap_ascending.trace_base`

--- rust/node/tests/tests/bootstrap_ascending.rs
+++ rust/node/tests/tests/bootstrap_ascending.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use rsnano_core::{Amount, BlockEnum, StateBlock, DEV_GENESIS_KEY};
+use rsnano_core::{Amount, BlockEnum, BlockHash, KeyPair, StateBlock, DEV_GENESIS_KEY};
 use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH, DEV_GENESIS_PUB_KEY};
 use test_helpers::{assert_timely, System};
 
@@ -55,5 +55,40 @@ fn account_inductive() {
     let node1 = system.make_node();
     assert_timely(Duration::from_secs(50), || {
         node1.block_exists(&send2.hash())
+    });
+}
+
+/**
+ * Tests that bootstrap_ascending will return multiple new blocks in-order
+ */
+
+#[test]
+fn trace_base() {
+    let mut system = System::new();
+    let node0 = system.make_node();
+    let key = KeyPair::new();
+    let send1 = BlockEnum::State(StateBlock::new(
+        *DEV_GENESIS_ACCOUNT,
+        *DEV_GENESIS_HASH,
+        *DEV_GENESIS_PUB_KEY,
+        Amount::MAX - Amount::raw(1),
+        key.public_key().as_account().into(),
+        &DEV_GENESIS_KEY,
+        node0.work_generate_dev((*DEV_GENESIS_HASH).into()),
+    ));
+    let receive1 = BlockEnum::State(StateBlock::new(
+        key.public_key().as_account(),
+        BlockHash::zero(),
+        *DEV_GENESIS_PUB_KEY,
+        Amount::raw(1),
+        send1.hash().into(),
+        &key,
+        node0.work_generate_dev(key.public_key().as_account().into()),
+    ));
+    node0.process(send1).unwrap();
+    node0.process(receive1.clone()).unwrap();
+    let node1 = system.make_node();
+    assert_timely(Duration::from_secs(10), || {
+        node1.block_exists(&receive1.hash())
     });
 }

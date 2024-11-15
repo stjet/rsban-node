5e1dd94fdc6ecd96bc0c522211233ab4ed000ff1
Gustav Schauwecker <service@simpago.com>
2024-11-15 07:44:51 +0100

    Port test `conflicts.add_existing`

--- nano/core_test/conflicts.cpp
+++ nano/core_test/conflicts.cpp
@@ -14,56 +14,6 @@
 
 using namespace std::chrono_literals;
 
-TEST (conflicts, add_existing)
-{
-	nano::test::system system{ 1 };
-	auto & node1 = *system.nodes[0];
-	nano::keypair key1;
-
-	// create a send block to send all of the nano supply to key1
-	nano::block_builder builder;
-	auto send1 = builder
-				 .send ()
-				 .previous (nano::dev::genesis->hash ())
-				 .destination (key1.pub)
-				 .balance (0)
-				 .sign (nano::dev::genesis_key.prv, nano::dev::genesis_key.pub)
-				 .work (0)
-				 .build ();
-	node1.work_generate_blocking (*send1);
-
-	// add the block to ledger as an unconfirmed block
-	ASSERT_EQ (nano::block_status::progress, node1.process (send1));
-
-	// wait for send1 to be inserted in the ledger
-	ASSERT_TIMELY (5s, node1.block (send1->hash ()));
-
-	// instruct the election scheduler to trigger an election for send1
-	nano::test::start_election (system, node1, send1->hash ());
-
-	// wait for election to be started before processing send2
-	ASSERT_TIMELY (5s, node1.active.active (*send1));
-
-	nano::keypair key2;
-	auto send2 = builder
-				 .send ()
-				 .previous (nano::dev::genesis->hash ())
-				 .destination (key2.pub)
-				 .balance (0)
-				 .sign (nano::dev::genesis_key.prv, nano::dev::genesis_key.pub)
-				 .work (0)
-				 .build ();
-	node1.work_generate_blocking (*send2);
-	send2->sideband_set ({});
-
-	// the block processor will notice that the block is a fork and it will try to publish it
-	// which will update the election object
-	node1.block_processor.add (send2);
-
-	ASSERT_TRUE (node1.active.active (*send1));
-	ASSERT_TIMELY (5s, node1.active.active (*send2));
-}
-
 TEST (conflicts, add_two)
 {
 	nano::test::system system{};
--- rust/node/tests/tests/conflicts.rs
+++ rust/node/tests/tests/conflicts.rs
@@ -1,6 +1,10 @@
-use rsnano_core::{Amount, BlockEnum, KeyPair, StateBlock, DEV_GENESIS_KEY};
+use std::time::Duration;
+
+use rsnano_core::{Amount, BlockEnum, BlockSideband, KeyPair, StateBlock, DEV_GENESIS_KEY};
 use rsnano_ledger::{DEV_GENESIS_ACCOUNT, DEV_GENESIS_HASH, DEV_GENESIS_PUB_KEY};
-use test_helpers::{start_election, System};
+use rsnano_network::ChannelId;
+use rsnano_node::block_processing::BlockSource;
+use test_helpers::{assert_timely, start_election, System};
 
 #[test]
 fn start_stop() {
@@ -21,4 +25,52 @@ fn start_stop() {
     let election1 = start_election(&node1, &send1.hash());
     assert_eq!(node1.active.len(), 1);
     assert_eq!(election1.vote_count(), 1);
+}
+
+#[test]
+fn add_existing() {
+    let mut system = System::new();
+    let node1 = system.make_node();
+    let key1 = KeyPair::new();
+
+    // create a send block to send all of the nano supply to key1
+    let send1 = BlockEnum::State(StateBlock::new(
+        *DEV_GENESIS_ACCOUNT,
+        *DEV_GENESIS_HASH,
+        *DEV_GENESIS_PUB_KEY,
+        Amount::zero(),
+        key1.public_key().as_account().into(),
+        &DEV_GENESIS_KEY,
+        node1.work_generate_dev((*DEV_GENESIS_HASH).into()),
+    ));
+
+    // add the block to ledger as an unconfirmed block
+    node1.process(send1.clone()).unwrap();
+
+    // instruct the election scheduler to trigger an election for send1
+    start_election(&node1, &send1.hash());
+
+    // wait for election to be started before processing send2
+    assert_timely(Duration::from_secs(5), || node1.active.active(&send1));
+
+    let key2 = KeyPair::new();
+    let mut send2 = BlockEnum::State(StateBlock::new(
+        *DEV_GENESIS_ACCOUNT,
+        *DEV_GENESIS_HASH,
+        *DEV_GENESIS_PUB_KEY,
+        Amount::zero(),
+        key2.public_key().as_account().into(),
+        &DEV_GENESIS_KEY,
+        node1.work_generate_dev((*DEV_GENESIS_HASH).into()),
+    ));
+    send2.set_sideband(BlockSideband::new_test_instance());
+
+    // the block processor will notice that the block is a fork and it will try to publish it
+    // which will update the election object
+    node1
+        .block_processor
+        .add(send2.clone().into(), BlockSource::Live, ChannelId::LOOPBACK);
+
+    assert!(node1.active.active(&send1));
+    assert_timely(Duration::from_secs(5), || node1.active.active(&send2));
 }

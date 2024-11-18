2c10b5d576f63d9290b37cdc88003d4162e0ecf7
Gustav Schauwecker <service@simpago.com>
2024-11-18 15:11:09 +0100

    Reorganize folders

--- rust/run_tests_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if cargo test --lib -q "$@"
-then
-	play -q ../sounds/success.ogg&
-else 
-	play -q ../sounds/failed.ogg&
-fi
---
+++ util/run_tests_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if cargo test --lib -q "$@"
+then
+	play -q sounds/success.ogg&
+else 
+	play -q sounds/failed.ogg&
+fi

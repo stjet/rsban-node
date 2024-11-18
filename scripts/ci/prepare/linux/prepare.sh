3ae62d245740d2a6dd01e9a9fdec9547e874c8fd
Gustav Schauwecker <service@simpago.com>
2024-11-18 15:46:53 +0100

    Fix docker build

--- scripts/ci/prepare/linux/prepare.sh
+++ scripts/ci/prepare/linux/prepare.sh
@@ -10,29 +10,12 @@ DEBIAN_FRONTEND=noninteractive apt-get update -qq
 
 DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
 build-essential \
-g++ \
 curl \
 wget \
-curl \
 python3 \
-zlib1g-dev \
-cmake \
 git \
-valgrind \
 libssl-dev \
 pkg-config
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 
-pushd ..
-mkdir corrosion
-pushd corrosion
-git clone https://github.com/AndrewGaspar/corrosion.git \
-    && cmake -Scorrosion -Bbuild -DCMAKE_BUILD_TYPE=Release \
-    && cmake --build build --config Release \
-    && cmake --install build --config Release 
-popd
-popd
-
-# Compiler specific setup
-$(dirname "$BASH_SOURCE")/prepare-${COMPILER}.sh
--- scripts/ci/prepare/macos/prepare.sh
+++ scripts/ci/prepare/macos/prepare.sh
@@ -6,9 +6,3 @@ for i in {1..5}; do brew update && break || { echo "Update failed, retrying...";
 
 brew install coreutils
 
-pushd ..
-git clone https://github.com/corrosion-rs/corrosion.git
-cmake -Scorrosion -Bcorrosion_build -DCMAKE_BUILD_TYPE=Release
-cmake --build corrosion_build --config Release
-sudo cmake --install corrosion_build --config Release
-popd

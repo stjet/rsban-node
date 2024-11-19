52673991d5c0ed064afc0a9031c83b29cd9d3308
Gustav Schauwecker <service@simpago.com>
2024-11-19 10:34:37 +0100

    Fix incorrect implementation of RPC command raw_to_nano

--- rpc_server/src/command_handler/utils/raw_to_nano.rs
+++ rpc_server/src/command_handler/utils/raw_to_nano.rs
@@ -1,6 +1,23 @@
-use rsnano_core::{Amount, MXRB_RATIO};
+use rsnano_core::Amount;
 use rsnano_rpc_messages::AmountRpcMessage;
 
 pub fn raw_to_nano(args: AmountRpcMessage) -> AmountRpcMessage {
-    AmountRpcMessage::new(Amount::nano(args.amount.number() / *MXRB_RATIO))
+    AmountRpcMessage::new(Amount::raw(args.amount.number() / Amount::nano(1).number()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_raw_to_nano() {
+        assert_eq!(
+            raw_to_nano(AmountRpcMessage::new(Amount::raw(
+                12_400_000_000_000_000_000_000_000_000_000
+            )))
+            .amount
+            .number(),
+            12
+        );
+    }
 }

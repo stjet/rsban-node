2a688aeeddfbb7a9e60bd9024ca493f2d17b6e8c
ArTombado <45319004+ArTombado@users.noreply.github.com>
2024-12-02 20:51:28 -0300

    Fix unchecked count in block_count RPC

--- rpc_server/src/command_handler/ledger/block_count.rs
+++ rpc_server/src/command_handler/ledger/block_count.rs
@@ -4,7 +4,7 @@ use rsnano_rpc_messages::BlockCountResponse;
 impl RpcCommandHandler {
     pub(crate) fn block_count(&self) -> BlockCountResponse {
         let count = self.node.ledger.block_count();
-        let unchecked = self.node.unchecked.buffer_count() as u64;
+        let unchecked = self.node.unchecked.len() as u64;
         let cemented = self.node.ledger.cemented_count();
         BlockCountResponse {
             count: count.into(),

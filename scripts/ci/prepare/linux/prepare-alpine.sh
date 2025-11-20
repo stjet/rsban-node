30249b5573a2b0f5b867e56d22c8a191b6303f1f
itsnotuctecat <jelly-extrude-atom@duck.com>
2025-11-20 22:01:09 +0000

    Alpine dockerfile and logo

---
+++ scripts/ci/prepare/linux/prepare-alpine.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euox pipefail
+
+COMPILER=${COMPILER:-gcc}
+echo "Compiler: '${COMPILER}'"
+
+apk update
+apk add --no-cache \
+    build-base \
+    curl \
+    wget \
+    python3 \
+    git \
+    openssl-dev \
+    pkgconfig
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y

7ef51684e1a48f40c6651f5fdbac9a81e51f9056
itsnotuctecat <jelly-extrude-atom@duck.com>
2025-11-23 04:58:27 +0800

    Update prepare-alpine.sh
    GCC compiler only

--- scripts/ci/prepare/linux/prepare-alpine.sh
+++ scripts/ci/prepare/linux/prepare-alpine.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -euox pipefail
 
 COMPILER=${COMPILER:-gcc}
@@ -12,6 +12,10 @@ apk add --no-cache \
     python3 \
     git \
     openssl-dev \
+    bash \
+    musl-dev \
+    cmake \
+    linux-headers \
     pkgconfig
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
